79404f3e95542df03bb82aa1bb81c1385f06bdcd
Anthony Dillon <me@anthonydillon.com>
2015-02-23 17:01:58 +0000

    Fixed watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,38 @@
-// Include gulp
-// to install the plugins, run the following:
-// npm install gulp-rename gulp-ruby-sass gulp-autoprefixer gulp-cache gulp-scss-lint gulp-minify-css gulp-notify --save-dev
 
 var gulp = require('gulp')
     rename = require('gulp-rename'),
-    sass = require('gulp-ruby-sass'),
+    sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     notify = require('gulp-notify'),
     scsslint = require('gulp-scss-lint');
     minifycss = require('gulp-minify-css');
 
-// Define base folders
-var src = 'sass/';
-var dest = 'css/';
 
-// Compile CSS from Sass files
 gulp.task('sass', function() {
-    return sass(src + 'ubuntu-styles.scss', { style: 'expanded' })
+    return gulp.src('scss/ubuntu-styles.scss')
         .pipe(scsslint())
+        .pipe(sass({ style: 'expanded' }))
         .on('error', function (err) { console.log(err.message); })
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-        .pipe(gulp.dest(dest))
+        .pipe(gulp.dest('css/'))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
-        .pipe(gulp.dest(dest))
-        .pipe(notify({ message: 'Great news! your task is complete' }));
+        .pipe(gulp.dest('css/'))
+        .pipe(notify({ message: 'Great news! your build is complete' }));
 });
 
-// watch for changes in files
+gulp.task('sass-lite', function() {
+    return gulp.src('scss/ubuntu-styles.scss')
+        .pipe(sass({ style: 'expanded' }))
+        .on('error', function (err) { console.log(err.message); })
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+        .pipe(gulp.dest('css/'));
+});
+
+
 gulp.task('watch', function() {
-  // Watch .scss files
-  gulp.watch(src + '*.scss', ['sass']);
+    gulp.watch('scss/*.scss', ['sass-lite']);
 });
 
 
-// default task
 gulp.task('default', ['sass']);
