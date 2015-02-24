3e1f1a0650b89ad654503ab8cf8072bb36f0ca1e
Anthony Dillon <me@anthonydillon.com>
2015-02-24 15:55:11 +0000

    Add sassdoc to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,33 +6,46 @@ var gulp = require('gulp')
     notify = require('gulp-notify'),
     scsslint = require('gulp-scss-lint');
     minifycss = require('gulp-minify-css');
+    sassdoc = require('sassdoc');
+
+
+gulp.task('help', function() {
+    console.log('sass - Generate the min and unminified css from sass');
+    console.log('docs - Generate the docs from the source sass');
+    console.log('build - Generate css and docs');
+    console.log('watch - Watch sass files and generate unminified css');
+});
 
 
 gulp.task('sass', function() {
-    return gulp.src('scss/*.scss')
+    return gulp.src('scss/ubuntu-styles.scss')
         .pipe(scsslint())
         .pipe(sass({ style: 'expanded' }))
         .on('error', function (err) { console.log(err.message); })
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-        .pipe(gulp.dest('css/'))
+        .pipe(gulp.dest('build/css/'))
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
-        .pipe(gulp.dest('css/'))
-        .pipe(notify({ message: 'Great news! your build is complete' }));
+        .pipe(gulp.dest('build/css/'))
 });
+
+gulp.task('docs', function() {
+    return gulp.src('scss/**/*.scss')
+        .pipe(sassdoc({ 'dest': 'build/docs'}));
+});
+
+gulp.task('build', ['sass', 'docs']);
 
 gulp.task('sass-lite', function() {
     return gulp.src('scss/ubuntu-styles.scss')
         .pipe(sass({ style: 'expanded' }))
         .on('error', function (err) { console.log(err.message); })
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest('build/css/'));
 });
-
 
 gulp.task('watch', function() {
     gulp.watch('scss/*.scss', ['sass-lite']);
 });
 
-
-gulp.task('default', ['sass']);
+gulp.task('default', ['help']);
