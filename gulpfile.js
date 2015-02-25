d911cbe53c385cb37cfef57da901c00224be7b77
Graham Bancroft <gbancroft@gmail.com>
2015-02-25 11:40:14 +0000

    SCSS linti fixes and add further lint config

--- gulpfile.js
+++ gulpfile.js
@@ -33,12 +33,12 @@ gulp.task('help', function() {
 
 gulp.task('sasslint', function() {
     return gulp.src('scss/**/*.scss')
-        .pipe(scsslint())
+        .pipe(scsslint({ 'config' : 'lint.yml' }))
         .pipe(scsslint.failReporter());
 });
 
 gulp.task('sass', function() {
-    return gulp.src('scss/*.scss')
+    return gulp.src('scss/**/*.scss')
         .pipe(sass({
             style: 'expanded',
             onError: throwSassError
