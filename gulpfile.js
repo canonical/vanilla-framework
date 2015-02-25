7a3f776ed6e30b757e9eb7e6337d744445b68cb9
Robin Winslow <robin@robinwinslow.co.uk>
2015-02-25 10:59:57 +0000

    Throw better Sass errors
    - The Gulp Sass target will now return an error status, with a useful error message
    - The sass-lite target will return an error in a simpler and more useful way (without breaking watch)

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
-
 var gulp = require('gulp')
     rename = require('gulp-rename'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     notify = require('gulp-notify'),
+    gutil = require('gulp-util'),
     scsslint = require('gulp-scss-lint');
     minifycss = require('gulp-minify-css');
-    sassdoc = require('sassdoc');
+    sassdoc = require('sassdoc'),
+    util = require('util');
 
+/* Helper functions */
+function throwSassError(sassError) {
+    throw new gutil.PluginError({
+        plugin: 'sass',
+        message: util.format(
+            "Sass error: '%s' on line %s of %s",
+            sassError.message,
+            sassError.line,
+            sassError.file
+        )
+    });
+}
 
+/* Gulp instructions start here */
 gulp.task('help', function() {
     console.log('sass - Generate the min and unminified css from sass');
     console.log('docs - Generate the docs from the source sass');
@@ -25,8 +39,10 @@ gulp.task('sasslint', function() {
 
 gulp.task('sass', function() {
     return gulp.src('scss/*.scss')
-        .pipe(sass({ style: 'expanded' }))
-        .on('error', function (err) { console.log(err.message); })
+        .pipe(sass({
+            style: 'expanded',
+            onError: throwSassError
+        }))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
         .pipe(gulp.dest('build/css/'))
         .pipe(rename({suffix: '.min'}))
@@ -43,8 +59,7 @@ gulp.task('build', ['sasslint', 'sass', 'docs']);
 
 gulp.task('sass-lite', function() {
     return gulp.src('scss/ubuntu-styles.scss')
-        .pipe(sass({ style: 'expanded' }))
-        .on('error', function (err) { console.log(err.message); })
+        .pipe(sass({ style: 'expanded', errLogToConsole: true }))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
         .pipe(gulp.dest('build/css/'));
 });
