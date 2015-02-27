4afa13ce4954dd2fa2c886977bfdee93c1fc53ab
Robin Winslow <robin@robinwinslow.co.uk>
2015-02-27 16:41:00 +0000

    Install sass as well

--- .install_dependencies.sh
+++ .install_dependencies.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-echo "Installing gulp deps..."
+echo "Installing gulp dependencies..."
 type gulp || sudo npm install -g gulp
-type gem || sudo apt install ruby-dev || echo "gem missing - please isntall manually!" && type gem
+type gem || sudo apt install ruby-dev || echo "gem missing - please install manually!" && type gem
+type sass || sudo gem install sass || echo "sass missing - please install manually!" && type sass
 
 if ! type scss-lint 2> /dev/null || [[ $(scss-lint --version | egrep -o "[0-9]+\.[0-9]+\.[0-9]+") < 0.34 ]]; then
 	sudo gem install scss-lint
