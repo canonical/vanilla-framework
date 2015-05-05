1c845cfd48230735a7fbfe4902ee485a0730d687
Anthony Dillon <me@anthonydillon.com>
2015-05-05 16:44:10 +0100

    Add input field states

--- gulpfile.js
+++ gulpfile.js
@@ -42,6 +42,7 @@ gulp.task('sass', function() {
     return gulp.src('scss/**/*.scss')
         .pipe(sass({
             style: 'expanded',
+            errLogToConsole: true,
             onError: throwSassError
         }))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
