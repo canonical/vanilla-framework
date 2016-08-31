55e28b3dc6eaa6933fa8a6507f4919c7a0622c1d
Robin Winslow <robin@robinwinslow.co.uk>
2016-08-31 14:17:52 +0100

    Look in scss directory

--- gulp/lint.js
+++ gulp/lint.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sassLint = require('gulp-sass-lint');
 
 gulp.task('lint:sass', function() {
-  return gulp.src('sass/**/*.s+(a|c)ss')
+  return gulp.src('scss/**/*.s+(a|c)ss')
     .pipe(sassLint())
     .pipe(sassLint.format())
     .pipe(sassLint.failOnError())
