fdb83d773cedd3f1bf389307bd06d61ea0b8e494
Anthony Dillon <me@anthonydillon.com>
2017-01-30 16:27:40 +0000

    Update mkspell dir path

--- gulp/lint.js
+++ gulp/lint.js
@@ -10,7 +10,7 @@ gulp.task('lint:sass', function() {
 });
 
 gulp.task('lint:spellcheck', function(cb) {
-  return exec('mdspell docs/**/*.md -r -n -a --en-gb', function (err, stdout, stderr) {
+  return exec('node_modules/markdown-spellcheck/bin/mdspell docs/**/*.md -r -n -a --en-gb', function (err, stdout, stderr) {
     console.log(stdout);
     cb(err);
   });
