5639d2167d48f9a568feb7dc5ded988f36505efb
Barry McGee <barry.mcgee@integralcode.co.uk>
2017-01-31 17:37:28 +0000

    Add jekyll to gulp workflow

---
+++ gulp/jekyll.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+
+// Watch tasks
+gulp.task('jekyll', shell.task([
+  'jekyll serve'
+]))
--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,6 @@ gulp.task('help', function() {
 
 /* Gulp default task list */
 gulp.task('default', ['help']);
-gulp.task('develop', ['watch', 'sass:develop']);
+gulp.task('develop', ['watch', 'sass:develop', 'jekyll']);
 gulp.task('test', ['lint:sass', 'lint:spellcheck']);
 gulp.task('build', ['lint:sass', 'sass:build']);
