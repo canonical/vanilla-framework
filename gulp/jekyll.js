dacce48de9698caf81de47f350b3ebf87501bf0d
Robin Winslow <robin@robinwinslow.co.uk>
2017-02-06 08:53:41 +0000

    Update jekyll gulp task

--- gulp/jekyll.js
+++ gulp/jekyll.js
@@ -3,7 +3,6 @@ var shell = require('gulp-shell');
 
 // Watch tasks
 gulp.task('jekyll:serve', shell.task([
-  'ln -sf ../build/css docs/.',
-  'JEKYLL_ENV=development bundle exec jekyll serve -s docs'
+  'JEKYLL_ENV=development bundle exec jekyll serve'
 ]));
 
