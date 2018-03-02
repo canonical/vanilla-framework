24d2333918648c8caa373519c31abaa35c59afb6
Anthony Dillon <me@anthonydillon.com>
2018-03-02 14:49:11 +0000

    Simplify the logic

--- tests/file-size-lint.sh
+++ tests/file-size-lint.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
+
+THRESHOLD=400000
 FILE='build/css/build.min.css'
+
 if [ ! -f $FILE ]; then
   echo 'ERROR: Build file could not be found or is not readable. Please build first with `./run build`'
   exit 1
 fi
 
-THRESHOLD=400000
 CURRENTSIZE=$(du -b $FILE | cut -f 1)
-MESSAGE="Your generated CSS is ${CURRENTSIZE} with a threshold of ${THRESHOLD}"
-if [ $CURRENTSIZE -lt $THRESHOLD ]; then
-  STATUS='SUCCESS'
-  EXIT=0
-else
-  STATUS='ERROR'
-  EXIT=1
+if [ ${CURRENTSIZE} -gt ${THRESHOLD} ]; then
+  echo "ERROR: Your CSS is over ${THRESHOLD} (it's ${CURRENTSIZE})"
+  exit 1
 fi
 
-echo "${STATUS}: ${MESSAGE}"
-exit $EXIT
+echo "Success: your generated CSS is ${CURRENTSIZE} (under ${THRESHOLD})"
