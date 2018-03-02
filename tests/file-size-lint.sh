ddb4d709d0deba55c3fc8e1898809e51274a2771
Anthony Dillon <me@anthonydillon.com>
2018-03-02 12:54:31 +0000

    Tidy up the if statements

--- tests/file-size-lint.sh
+++ tests/file-size-lint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 FILE='build/css/build.min.css'
-if [ ! -f $FILE ]
-then
+if [ ! -f $FILE ]; then
   echo 'ERROR: Build file could not be found or is not readable. Please build first with `./run build`'
   exit 1
 fi
@@ -11,8 +10,7 @@ CURRENTSIZE=$(du -b $FILE | cut -f 1)
 REPONSESTART='Your generated CSS is '
 REPONSEJOIN=' with a threshold of '
 MESSAGE=$REPONSESTART$CURRENTSIZE$REPONSEJOIN$THRESHOLD
-if [ $CURRENTSIZE -lt $THRESHOLD ]
-then
+if [ $CURRENTSIZE -lt $THRESHOLD ]; then
   STATUS='SUCCESS: '
   EXIT=0
 else
