617d3e6993db19058c124db0cffe446158a39684
Lyubomir Popov <lyubomir-popov@gmail.com>
2018-03-25 20:21:21 +0100

    Fix #1495 - reset now appears at top of build.css

--- gulp/styles.js
+++ gulp/styles.js
@@ -2,10 +2,11 @@ var
 gulp = require('gulp'),
 sass = require('gulp-sass'),
 autoprefixer = require('gulp-autoprefixer'),
+concat = require('gulp-concat'),
 cssnano = require('gulp-cssnano'),
 rename = require('gulp-rename'),
 sourcemaps = require('gulp-sourcemaps'),
-sassPath = 'scss/*.scss';
+sassPath = ['scss/reset.scss', 'scss/build.scss'];
 
 // Provide details of Sass errors
 function throwSassError(sassError) {
@@ -26,6 +27,8 @@ gulp.task('sass:develop', function() {
     .pipe(sourcemaps.init())
     .pipe(sass({ style: 'expanded', errLogToConsole: true }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(sourcemaps.init())
+    .pipe(concat('build.css'))
     .pipe(sourcemaps.write('maps/'))
     .pipe(gulp.dest('build/css/'))
 });
@@ -42,6 +45,7 @@ gulp.task('sass:build', function() {
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
     .pipe(gulp.dest('build/css/'))
     .pipe(cssnano())
+    .pipe(concat('build.css'))
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('build/css/'));
 });
