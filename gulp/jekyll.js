9de1f28e92fa5ced33efdeff000e1bcd8160bb3f
Barry McGee <barry.mcgee@integralcode.co.uk>
2018-10-08 16:47:40 +0100

    Prettier all the things ✨

--- gulp/jekyll.js
+++ gulp/jekyll.js
@@ -2,12 +2,7 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 
 // Watch tasks
-gulp.task('jekyll:serve', shell.task([
-  'JEKYLL_ENV=development bundle exec jekyll serve'
-]));
+gulp.task('jekyll:serve', shell.task(['JEKYLL_ENV=development bundle exec jekyll serve']));
 
 // Build jekyll
-gulp.task('jekyll:build', shell.task([
-  'JEKYLL_ENV=development bundle exec jekyll build'
-]));
-
+gulp.task('jekyll:build', shell.task(['JEKYLL_ENV=development bundle exec jekyll build']));
