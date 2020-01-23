d4680de5402ec9121453fd6541f60441762ba099
Bartek Szopka <bartek.szopka@canonical.com>
2020-01-23 17:37:57 +0100

    Get all the links to snapshot

--- snapshots.js
+++ snapshots.js
@@ -1,19 +1,13 @@
 const PercyScript = require('@percy/script');
-
-const URLS = [
-  'http://localhost:8101/',
-  'http://localhost:8101/building-vanilla/',
-  'http://localhost:8101/customising-vanilla/',
-  'http://localhost:8101/examples/',
-  'http://localhost:8101/examples/examples/base/abbr/',
-  'http://localhost:8101/examples/patterns/accordion/'
-];
+const GetSiteUrls = require('get-site-urls');
 
 PercyScript.run(async (page, percySnapshot) => {
-  for (var i = 0; i < URLS.length; i++) {
-    await page.goto(URLS[i]);
+  const links = await GetSiteUrls('http://localhost:8101/');
+
+  for (var i = 0; i < links.pages.length; i++) {
+    await page.goto(links.pages[i]);
     // ensure the page has loaded before capturing a snapshot
     await page.waitFor('body');
-    await percySnapshot(URLS[i]);
+    await percySnapshot(links.pages[i]);
   }
 });
