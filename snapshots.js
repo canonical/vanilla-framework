96ea154c5f0e07bf9e4ab67cf4a3334f98c40e34
Bartek Szopka <bartek.szopka@canonical.com>
2020-01-23 17:16:26 +0100

    Snapshot a list of URLs

--- snapshots.js
+++ snapshots.js
@@ -1,8 +1,19 @@
 const PercyScript = require('@percy/script');
 
+const URLS = [
+  'http://localhost:8101/',
+  'http://localhost:8101/building-vanilla/',
+  'http://localhost:8101/customising-vanilla/',
+  'http://localhost:8101/examples/',
+  'http://localhost:8101/examples/examples/base/abbr/',
+  'http://localhost:8101/examples/patterns/accordion/'
+];
+
 PercyScript.run(async (page, percySnapshot) => {
-  await page.goto('http://localhost:8101/');
-  // ensure the page has loaded before capturing a snapshot
-  await page.waitFor('h1');
-  await percySnapshot('/index.html');
+  for (var i = 0; i < URLS.length; i++) {
+    await page.goto(URLS[i]);
+    // ensure the page has loaded before capturing a snapshot
+    await page.waitFor('body');
+    await percySnapshot(URLS[i]);
+  }
 });
