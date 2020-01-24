326f065b8a3da3ed425901c2a2553223f1ae4c70
Bartek Szopka <bartek.szopka@canonical.com>
2020-01-24 09:44:56 +0100

    Snapshot script updates

--- snapshots.js
+++ snapshots.js
@@ -1,13 +1,20 @@
 const PercyScript = require('@percy/script');
 const GetSiteUrls = require('get-site-urls');
 
+const PORT = process.env.PORT || 8101;
+
 PercyScript.run(async (page, percySnapshot) => {
-  const links = await GetSiteUrls('http://localhost:8101/');
+  const links = await GetSiteUrls(`http://localhost:${PORT}/`);
 
   for (var i = 0; i < links.pages.length; i++) {
-    await page.goto(links.pages[i]);
+    const url = links.pages[i];
+    await page.goto(url);
+
     // ensure the page has loaded before capturing a snapshot
     await page.waitFor('body');
-    await percySnapshot(links.pages[i]);
+
+    // create percy snapshot with path as a name
+    const path = new URL(url).pathname.replace(/\/?$/, '/');
+    await percySnapshot(path, {widths: [375, 1280]});
   }
 });
