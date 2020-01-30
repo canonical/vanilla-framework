33e545fe04ad67ab4eab2d8e002bb94a19ea0485
Bartek Szopka <bartek.szopka@canonical.com>
2020-01-30 22:26:01 +0100

    Add URLs to standalone examples to percy

--- snapshots.js
+++ snapshots.js
@@ -4,7 +4,18 @@ const GetSiteUrls = require('get-site-urls');
 const PORT = process.env.PORT || 8101;
 
 PercyScript.run(async (page, percySnapshot) => {
-  const links = await GetSiteUrls(`http://localhost:${PORT}/`);
+  const links = await GetSiteUrls(`http://localhost:${PORT}`);
+
+  // add links to standalone examples for base and patterns
+  let urls = links.pages.flatMap(url => {
+    if (url.includes('/examples/base/') || url.includes('/examples/patterns/')) {
+      return [url, `${url}?standalone`];
+    } else {
+      return url;
+    }
+  });
+
+  urls.sort();
 
   // disable JS requests to prevent CodePen from rendering examples
   await page.setRequestInterception(true);
@@ -13,15 +24,15 @@ PercyScript.run(async (page, percySnapshot) => {
     else request.continue();
   });
 
-  for (var i = 0; i < links.pages.length; i++) {
-    const url = links.pages[i];
-    await page.goto(url);
+  for (var i = 0; i < urls.length; i++) {
+    const url = new URL(urls[i]);
+    await page.goto(url.href);
 
     // ensure the page has loaded before capturing a snapshot
     await page.waitFor('body');
 
-    // create percy snapshot with path as a name
-    const path = new URL(url).pathname.replace(/\/?$/, '/');
+    // create percy snapshot with path (and optional params) as a name
+    const path = `${url.pathname}${url.search}`;
     await percySnapshot(path, {widths: [375, 1280]});
   }
 });
