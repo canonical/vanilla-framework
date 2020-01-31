dfa8a47f35289b3a60819452bf3186a67a5557fe
Robin Winslow <robin@robinwinslow.co.uk>
2020-01-31 07:22:54 +0000

    Stable URL for standalone examples
    Put standalone examples index page at /examples/standalone,
    and put all individual standalone example pages under
    /examples/standalone/{example_path}.

--- webapp/app.py
+++ webapp/app.py
@@ -23,33 +23,14 @@ app = FlaskBase(
 
 # Helpers
 # ===
-
-
-# Global context settings
-@app.context_processor
-def global_template_context():
-    with open("package.json") as package_json:
-        version = json.load(package_json)["version"]
-
-    return {
-        "version": version,
-        "current_year": datetime.datetime.now().year,
-        "path": flask.request.path,
-        "is_standalone": "standalone" in flask.request.args,
-    }
-
-
-template_finder_view = TemplateFinder.as_view("template_finder")
-
-
-@app.route("/examples")
-def examples_view():
+def _get_examples():
     example_files = glob.glob("docs/examples/*/**/*.html", recursive=True)
     examples = {}
 
     for filepath in sorted(example_files):
         # Remove "docs/examples/" prefix
-        example_path = filepath[len("docs/examples/") :]
+        prefix_length = len("docs/examples/")
+        example_path = filepath[prefix_length:]
         # Remove extension
         example_path = os.path.splitext(example_path)[0]
 
@@ -64,17 +45,47 @@ def examples_view():
             )
 
         examples.setdefault(outermost_parent, []).append(
-            {
-                "path": f"/examples/{example_path}",
-                "friendly_name": " / ".join(friendly_parts),
-            }
+            {"path": example_path, "friendly_name": " / ".join(friendly_parts)}
         )
 
-    show_standalone = "standalone" in flask.request.args
-    if show_standalone:
-        return flask.render_template("examples/standalone.html", examples=examples)
-    else:
-        return flask.render_template("examples/index.html", examples=examples)
+    return examples
+
+
+# Global context settings
+@app.context_processor
+def global_template_context():
+    with open("package.json") as package_json:
+        version = json.load(package_json)["version"]
+
+    return {
+        "version": version,
+        "current_year": datetime.datetime.now().year,
+        "path": flask.request.path,
+    }
+
+
+template_finder_view = TemplateFinder.as_view("template_finder")
+
+
+@app.route("/examples")
+def examples_index():
+    return flask.render_template(
+        "examples/index.html", examples=_get_examples()
+    )
+
+
+@app.route("/examples/standalone")
+def standalone_examples_index():
+    return flask.render_template(
+        "examples/standalone.html", examples=_get_examples()
+    )
+
+
+@app.route("/examples/standalone/<path:example_path>")
+def standalone_example(example_path):
+    return flask.render_template(
+        f"examples/{example_path}.html", is_standalone=True
+    )
 
 
 app.add_url_rule("/", view_func=template_finder_view)
