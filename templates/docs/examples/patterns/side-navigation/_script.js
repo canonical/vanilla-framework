7cc8803feeb3a608a876951b001fe97f350b489e
Bartek Szopka <bartek.szopka@canonical.com>
2020-03-18 15:40:01 +0100

    Adds script to make side navigation examples interactive

---
+++ templates/docs/examples/patterns/side-navigation/_script.js
@@ -0,0 +1,14 @@
+// small script to make the example interactive
+// not intended to be used in projects
+var links = [].slice.call(document.querySelectorAll('.p-side-navigation__link'));
+
+links.forEach(function(link) {
+  link.addEventListener('click', function() {
+    var active = [].slice.call(document.querySelectorAll('.is-active'));
+    active.forEach(function(link) {
+      link.classList.remove('is-active');
+    });
+    this.classList.add('is-active');
+    this.blur();
+  });
+});
