3aa85db3dd1d7af37006438ab63fc07434682488
Bartek Szopka <bartek.szopka@canonical.com>
2020-04-21 15:39:54 +0200

    Improve side navigation toggle script

--- templates/static/js/scripts.js
+++ templates/static/js/scripts.js
@@ -1,20 +1,100 @@
-// Add table of contents to side navigation on documentation pages
-
-// get all headings from page and add it to current highligted item in side navigation
+// Setup toggling of side navigation drawer
 (function() {
-  let list = document.createElement('ul');
+  // throttling function calls, by Remy Sharp
+  // http://remysharp.com/2010/07/21/throttling-function-calls/
+  var throttle = function(fn, delay) {
+    var timer = null;
+    return function() {
+      var context = this,
+        args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(function() {
+        fn.apply(context, args);
+      }, delay);
+    };
+  };
+
+  /**
+    Toggles the expanded/collapsed classed on side navigation element.
+
+    @param {HTMLElement} sideNavigation The side navigation element.
+    @param {Boolean} show Whether to show or hide the drawer.
+  */
+  function toggleDrawer(sideNavigation, show) {
+    if (sideNavigation) {
+      if (show) {
+        sideNavigation.classList.remove('is-collapsed');
+        sideNavigation.classList.add('is-expanded');
+
+        // disable scroll on body when drawer is open
+        document.body.style.overflow = 'hidden';
+      } else {
+        sideNavigation.classList.remove('is-expanded');
+        sideNavigation.classList.add('is-collapsed');
+
+        // enable scroll on body when drawer is open
+        document.body.style.overflow = null;
+      }
+    }
+  }
+
+  /**
+    Attaches event listeners for the side navigation toggles
+    @param {HTMLElement} sideNavigation The side navigation element.
+  */
+  function setupSideNavigation(sideNavigation) {
+    var toggles = [].slice.call(sideNavigation.querySelectorAll('.js-drawer-toggle'));
+
+    toggles.forEach(function(toggle) {
+      toggle.addEventListener('click', function(event) {
+        event.preventDefault();
+        var sideNav = document.getElementById(toggle.getAttribute('aria-controls'));
+
+        if (sideNav) {
+          toggleDrawer(sideNav, !sideNav.classList.contains('is-expanded'));
+        }
+      });
+    });
+
+    // improvements for side nav when resizing the window
+    var sideNav = document.querySelector('.p-side-navigation');
+    var drawerEl = document.querySelector('.p-side-navigation__drawer');
+
+    window.addEventListener(
+      'resize',
+      throttle(function() {
+        var drawerPosition = window.getComputedStyle(drawerEl).position;
+
+        // when screen size changes from mobile (fixed drawer) to large screen
+        // enable scroll on body and reset any styles added by opening the drawer
+        if (drawerPosition !== 'fixed') {
+          sideNav.classList.remove('is-expanded');
+          sideNav.classList.remove('is-collapsed');
+          document.body.style.overflow = null;
+        }
+      }, 200)
+    );
+  }
+
+  setupSideNavigation(document.querySelector('.p-side-navigation'));
+})();
+
+// Add table of contents to side navigation on documentation pages
+(function() {
+  // get all headings from page and add it to current highligted item in side navigation
+  var list = document.createElement('ul');
   list.classList.add('p-side-navigation__list');
 
-  let item = document.createElement('li');
+  var item = document.createElement('li');
   item.classList.add('p-side-navigation__item');
 
-  let anchor = document.createElement('a');
+  var anchor = document.createElement('a');
   anchor.classList.add('p-side-navigation__link');
 
   // Add all H3s with IDs to the table of contents list
   [].slice.call(document.querySelectorAll('main h3[id]')).forEach(function(heading) {
-    let thisItem = item.cloneNode();
-    let thisAnchor = anchor.cloneNode();
+    var thisItem = item.cloneNode();
+    var thisAnchor = anchor.cloneNode();
     thisAnchor.setAttribute('href', '#' + heading.id);
     thisAnchor.textContent = heading.textContent;
     thisItem.appendChild(thisAnchor);
@@ -23,7 +103,7 @@
 
   // Add table of contents as nested list to side navigation
   if (list.querySelectorAll('li').length > 0) {
-    let parent = document.querySelector('.p-side-navigation__link.is-active').parentNode;
+    var parent = document.querySelector('.p-side-navigation__link.is-active').parentNode;
 
     parent.appendChild(list);
   }
@@ -31,12 +111,12 @@
 
 // scroll active side navigation item into view (without scrolling whole page)
 (function() {
-  let sideNav = document.querySelector('.p-side-navigation');
-  let currentItem = document.querySelector('.p-side-navigation__link.is-active');
+  var sideNav = document.querySelector('.p-side-navigation');
+  var currentItem = document.querySelector('.p-side-navigation__link.is-active');
 
   // calculate scroll by comparing top of side nav and top of active item
-  let currentItemOffset = currentItem.getBoundingClientRect().top;
-  let offset = currentItemOffset - sideNav.getBoundingClientRect().top;
+  var currentItemOffset = currentItem.getBoundingClientRect().top;
+  var offset = currentItemOffset - sideNav.getBoundingClientRect().top;
 
   // only scroll if active link is off screen or close to bottom of the window
   if (currentItemOffset > window.innerHeight * 0.7) {
