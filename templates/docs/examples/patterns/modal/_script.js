9de52184a2424bc541ca18fa33d29b718657f113
Bartek Szopka <bartek.szopka@canonical.com>
2020-04-28 11:58:05 +0200

    Extract modal script to shared file

---
+++ templates/docs/examples/patterns/modal/_script.js
@@ -0,0 +1,21 @@
+/**
+  Toggles visibility of modal dialog.
+  @param {HTMLElement} modal Modal dialog to show or hide.
+*/
+function toggleModal(modal) {
+  if (modal && modal.classList.contains('p-modal')) {
+    if (modal.style.display === 'none') {
+      modal.style.display = 'flex';
+    } else {
+      modal.style.display = 'none';
+    }
+  }
+}
+
+// Add click handler for clicks on elements with aria-controls
+document.addEventListener('click', function (event) {
+  var targetControls = event.target.getAttribute('aria-controls');
+  if (targetControls) {
+    toggleModal(document.getElementById(targetControls));
+  }
+});
