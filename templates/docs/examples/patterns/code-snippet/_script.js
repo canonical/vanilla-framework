1184a0f113c45953b4444c91b3336b722577d515
Scott Mason Nash <sowasred2012@gmail.com>
2021-01-25 16:06:58 +0000

    add code snippet with dropdown example

---
+++ templates/docs/examples/patterns/code-snippet/_script.js
@@ -0,0 +1,34 @@
+function setupCodeSnippetOptions(dropdown) {
+  var dropdownLabel = dropdown.querySelector('span');
+  var targetMenu = document.getElementById(dropdown.getAttribute('aria-controls'));
+  var options = [].slice.call(targetMenu.querySelectorAll('a'));
+
+  options.forEach(function (option) {
+    option.addEventListener('click', function (e) {
+      e.preventDefault();
+      var targetPanel = document.getElementById(option.getAttribute('aria-controls'));
+
+      dropdownLabel.innerHTML = option.innerHTML;
+      togglePanel(targetPanel, options);
+    });
+  });
+}
+
+function togglePanel(targetPanel, options) {
+  options.forEach(function (option) {
+    var panel = document.getElementById(option.getAttribute('aria-controls'));
+    panel.classList.add('u-hide');
+  });
+
+  targetPanel.classList.remove('u-hide');
+}
+
+function setupCodeSnippetDropdowns(codeSnippetDropdownSelector) {
+  var dropdowns = [].slice.call(document.querySelectorAll(codeSnippetDropdownSelector));
+
+  dropdowns.forEach(function (dropdown) {
+    setupCodeSnippetOptions(dropdown);
+  });
+}
+
+setupCodeSnippetDropdowns('.p-code-snippet__toggle');
