cd9bec3d4813d5b4a75b83d5d725ae93c91142ff
Scott Mason Nash <sowasred2012@gmail.com>
2021-01-27 23:26:05 +0000

    add js comments to code snippet dropdown js

--- templates/docs/examples/patterns/code-snippet/_script.js
+++ templates/docs/examples/patterns/code-snippet/_script.js
@@ -1,3 +1,7 @@
+/**
+  Attaches change event listener to a given select.
+  @param {HTMLElement} dropdown Select element belonging to a code snippet.
+*/
 function attachDropdownEvents(dropdown) {
   dropdown.addEventListener('change', function (e) {
     var targetElement = document.getElementById(e.target.value);
@@ -6,6 +10,12 @@ function attachDropdownEvents(dropdown) {
   });
 }
 
+/**
+  Shows a given code snippet panel, and hides the rest.
+  @param {HTMLElement} targetPanel the panel to show.
+  @param {HTMLOptionsCollection} options collection of options
+  whose values reference panels to be hidden.
+*/
 function togglePanel(targetPanel, options) {
   for (var i = 0; i < options.length; i++) {
     var panel = document.getElementById(options[i].value);
@@ -17,6 +27,11 @@ function togglePanel(targetPanel, options) {
   targetPanel.setAttribute('aria-hidden', false);
 }
 
+/**
+  Attaches events to selects with a given classname.
+  @param {String} codeSnippetDropdownSelector class name of the select elements
+  we want to attach events to.
+*/
 function setupCodeSnippetDropdowns(codeSnippetDropdownSelector) {
   var dropdowns = [].slice.call(document.querySelectorAll(codeSnippetDropdownSelector));
 
