647f41d84d49092697c658f46513351ea0a7a7c9
Bartek Szopka <bartek.szopka@canonical.com>
2021-02-17 14:31:03 +0100

    Prototype of using code snippet and iframe for examples

--- templates/static/js/example.js
+++ templates/static/js/example.js
@@ -1,24 +1,4 @@
-/* global VANILLA_VERSION */
 (function () {
-  if (!window.VANILLA_VERSION) {
-    throw Error('VANILLA_VERSION not specified.');
-  }
-
-  var CODEPEN_PREFILL_CONFIG = {
-    title: 'Vanilla framework example',
-    head: "<meta name='viewport' content='width=device-width, initial-scale=1'>",
-    stylesheets: [
-      // link to latest Vanilla CSS
-      // if it's a demo, use local build.css for better QA, otherwise use latest published Vanilla
-      /demos\.haus$/.test(window.location.host)
-        ? `${window.location.origin}/static/build/css/build.css`
-        : 'https://assets.ubuntu.com/v1/vanilla-framework-version-' + VANILLA_VERSION + '.min.css',
-      // link to example stylesheet (to set margin on body)
-      'https://assets.ubuntu.com/v1/4653d9ba-example.css',
-    ],
-    tags: ['Vanilla framework'],
-  };
-
   var CODEPEN_HEIGHT = 400;
 
   document.addEventListener('DOMContentLoaded', function () {
@@ -52,8 +32,20 @@
     code.appendChild(document.createTextNode(source));
 
     var pre = document.createElement('pre');
+    pre.classList.add('p-code-snippet__block');
+    // FIXME: this doesn't work with multiple examples on a page, we need unique ids or another way of selecting panels to show/hide
+    pre.id = 'panel-' + lang;
+
+    // TODO: move max-height elsewhere to CSS?
+    pre.style.maxHeight = '300px';
+
+    if (lang !== 'html') {
+      pre.classList.add('u-hide');
+    }
+
     if (lang) {
       pre.setAttribute('data-lang', lang);
+      pre.classList.add('language-' + lang);
     }
 
     pre.appendChild(code);
@@ -72,58 +64,96 @@
     var htmlSource = stripScriptsFromSource(bodyHTML);
     var jsSource = getScriptFromSource(bodyHTML);
     var cssSource = getStyleFromSource(headHTML);
-    var externalScripts = getExternalScriptsFromSource(html);
 
     var height = placementElement.getAttribute('data-height') || CODEPEN_HEIGHT;
 
     var container = document.createElement('div');
 
-    // Set attributes of code block to be parsed by CodePen embed script
+    container.classList.add('p-code-snippet', 'codepen-example');
 
-    // hide code container by default, so it doesn't flash on page
-    // before CodePen is rendered
-    container.classList.add('codepen-example', 'u-hide');
+    var header = document.createElement('div');
+    header.classList.add('p-code-snippet__header');
+    var titleEl = document.createElement('h5');
+    titleEl.classList.add('p-code-snippet__title');
+    titleEl.innerText = title;
+    header.appendChild(titleEl);
 
-    // pass any external scripts to CodePen
-    if (externalScripts.length) {
-      CODEPEN_PREFILL_CONFIG.scripts = externalScripts;
-    }
-
-    var config = JSON.parse(JSON.stringify(CODEPEN_PREFILL_CONFIG));
-    // update title in the config with title of the example page
-    if (title) {
-      config.title = title;
-    }
-
-    container.setAttribute('data-prefill', encodeURI(JSON.stringify(config)));
-    container.setAttribute('data-height', height);
-    // For more options see CodePen docs
-    // https://blog.codepen.io/documentation/prefill-embeds/
-    //
-    // For example:
-    //container.setAttribute('data-theme-id', 'light');
-    //container.setAttribute("data-default-tab", "html,result");
+    container.appendChild(header);
 
     // Build code block structure
     container.appendChild(createPreCode(htmlSource, 'html'));
 
+    if (jsSource || cssSource) {
+      var dropdownsEl = document.createElement('div');
+      dropdownsEl.classList.add('p-code-snippet__dropdowns');
+      var selectEl = document.createElement('select');
+      selectEl.classList.add('p-code-snippet__dropdown');
+      dropdownsEl.appendChild(selectEl);
+      header.appendChild(dropdownsEl);
+      var optionHTML = document.createElement('option');
+      optionHTML.value = 'panel-html';
+      optionHTML.innerText = 'HTML';
+      selectEl.appendChild(optionHTML);
+    }
+
     if (jsSource) {
       container.appendChild(createPreCode(jsSource, 'js'));
+      var optionJS = document.createElement('option');
+      optionJS.value = 'panel-js';
+      optionJS.innerText = 'JS';
+      selectEl.appendChild(optionJS);
     }
 
     if (cssSource) {
       container.appendChild(createPreCode(cssSource, 'css'));
+      var optionCSS = document.createElement('option');
+      optionCSS.value = 'panel-css';
+      optionCSS.innerText = 'CSS';
+      selectEl.appendChild(optionCSS);
     }
 
     placementElement.parentNode.insertBefore(container, placementElement);
 
-    if (window.__CPEmbed) {
-      // init codepen after examples code is rendered
-      window.__CPEmbed('.codepen-example');
-    } else {
-      // or show the code blocks as a fallback
-      container.classList.remove('u-hide');
-    }
+    var iframe = renderIframe(container, html, height);
+
+    // FIXME - hacky way that depends on separate script
+    // TODO: setup dropdowns
+    // setupCodeSnippetDropdowns('.p-code-snippet__dropdown');
+
+    // TODO: trigger only on the embedded examples?
+    Prism.highlightAll();
+  }
+
+  function renderIframe(container, html, height) {
+    var iframe = document.createElement('iframe');
+    iframe.width = '100%';
+    iframe.height = height + 'px';
+    iframe.frameBorder = 0;
+    container.appendChild(iframe);
+    var doc = iframe.contentWindow.document;
+    doc.open();
+    doc.write(html);
+    doc.close();
+
+    return iframe;
+    // TODO:
+    // // Wait for content to load before determining height
+    // var resizeInterval = setInterval(
+    //   function() {
+    //     if (iframe.contentDocument.readyState == 'complete') {
+    //       // remove any residual margin
+    //       iframe.contentDocument.body.style.margin = 0;
+    //       // add padding to see shadows pattern shadows
+    //       iframe.contentDocument.body.style.padding = '.5rem .25rem';
+    //       // Add extra spacing to catch edge cases
+    //       const frameHeight = iframe.contentDocument.body.scrollHeight;
+    //       iframe.height = frameHeight + "px";
+    //       clearInterval(resizeInterval);
+    //     }
+    //   },
+    //   100
+    // );
+    // setTimeout(function() {clearInterval(resizeInterval);}, 2000);
   }
 
   function getStyleFromSource(source) {
@@ -149,15 +179,5 @@
     div.innerHTML = source;
     var script = div.querySelector('script');
     return script ? script.innerHTML.trim() : null;
-  }
-
-  function getExternalScriptsFromSource(source) {
-    var div = document.createElement('div');
-    div.innerHTML = source;
-    var scripts = div.querySelectorAll('script[src]');
-    scripts = [].slice.apply(scripts).map(function (s) {
-      return s.src;
-    });
-    return scripts;
   }
 })();
