22bb8fa36f6c6aa572d40d99cd89fc7dbbecd6cd
Anthony Dillon <me@anthonydillon.com>
2021-03-04 23:25:08 +0000

    Address review feedback

--- templates/docs/examples/patterns/search-and-filter/_script.js
+++ templates/docs/examples/patterns/search-and-filter/_script.js
@@ -16,7 +16,7 @@ function togglePanel(container, panel) {
 
 // Add click handler for clicks on elements with aria-controls
 [].slice.call(document.querySelectorAll('.p-search-and-filter')).forEach(function (pattern) {
-  var input = pattern.querySelector('.p-search-box__input');
+  var input = pattern.querySelector('.p-search-and-filter__input');
   var container = pattern.querySelector('.p-search-and-filter__search-container');
   input.addEventListener('blur', function (event) {
     var targetPanel = pattern.querySelector('.p-search-and-filter__panel');
