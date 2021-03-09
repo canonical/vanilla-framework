a1012adf28b4e37a09abdbcb8757360c1df24e19
Bartek Szopka <bartek.szopka@canonical.com>
2021-03-09 09:48:10 +0100

    Clean up app layout example JS

--- templates/docs/examples/layouts/application/_script.js
+++ templates/docs/examples/layouts/application/_script.js
@@ -1,62 +1,81 @@
-if (document.querySelector('.js-menu-toggle')) {
-  document.querySelector('.js-menu-toggle').addEventListener('click', function () {
-    document.querySelector('.l-navigation').classList.toggle('is-collapsed');
-  });
-}
+/*
+  Set up event handlers for application layout examples.
+  This is for demo purposes only. Real applications should implement it within their application code.
+*/
+function setupAppLayoutExamples() {
+  var aside = document.querySelector('.l-aside');
+  var navigation = document.querySelector('.l-navigation');
 
-if (document.querySelector('.js-menu-close')) {
-  document.querySelector('.js-menu-close').addEventListener('click', function (e) {
-    document.querySelector('.l-navigation').classList.add('is-collapsed');
-    document.activeElement.blur();
-  });
-}
+  var menuToggle = document.querySelector('.js-menu-toggle');
+  var menuClose = document.querySelector('.js-menu-close');
+  var menuPin = document.querySelector('.js-menu-pin');
+  var asideOpen = document.querySelector('.js-aside-open');
+  var asideClose = document.querySelector('.js-aside-close');
+  var asideResize = document.querySelectorAll('.js-aside-resize');
+  var asidePin = document.querySelector('.js-aside-pin');
 
-if (document.querySelector('.js-aside-open')) {
-  document.querySelector('.js-aside-open').addEventListener('click', function () {
-    document.querySelector('.l-aside').classList.remove('is-collapsed');
-  });
-}
+  if (menuToggle) {
+    menuToggle.addEventListener('click', function () {
+      navigation.classList.toggle('is-collapsed');
+    });
+  }
 
-if (document.querySelector('.js-aside-close')) {
-  document.querySelector('.js-aside-close').addEventListener('click', function () {
-    document.querySelector('.l-aside').classList.add('is-collapsed');
-  });
-}
+  if (menuClose) {
+    menuClose.addEventListener('click', function (e) {
+      navigation.classList.add('is-collapsed');
+      document.activeElement.blur();
+    });
+  }
 
-[].slice.call(document.querySelectorAll('.js-aside-resize')).forEach(function (button) {
-  button.addEventListener('click', function () {
-    button.dataset.resizeClass;
-    var panel = document.getElementById(button.getAttribute('aria-controls'));
-    if (panel) {
-      panel.classList.remove('is-narrow');
-      panel.classList.remove('is-medium');
-      panel.classList.remove('is-wide');
-      if (button.dataset.resizeClass) {
-        panel.classList.add(button.dataset.resizeClass);
+  if (asideOpen) {
+    asideOpen.addEventListener('click', function () {
+      aside.classList.remove('is-collapsed');
+    });
+  }
+
+  if (asideClose) {
+    asideClose.addEventListener('click', function () {
+      aside.classList.add('is-collapsed');
+    });
+  }
+
+  [].slice.call(asideResize).forEach(function (button) {
+    button.addEventListener('click', function () {
+      button.dataset.resizeClass;
+      var panel = document.getElementById(button.getAttribute('aria-controls'));
+      if (panel) {
+        panel.classList.remove('is-narrow');
+        panel.classList.remove('is-medium');
+        panel.classList.remove('is-wide');
+        if (button.dataset.resizeClass) {
+          panel.classList.add(button.dataset.resizeClass);
+        }
       }
-    }
+    });
   });
-});
 
-if (document.querySelector('.js-menu-pin')) {
-  document.querySelector('.js-menu-pin').addEventListener('click', function () {
-    document.querySelector('.l-navigation').classList.toggle('is-pinned');
-    if (document.querySelector('.l-navigation').classList.contains('is-pinned')) {
-      document.querySelector('.js-menu-pin').querySelector('i').classList.remove('p-icon--pin');
-    } else {
-      document.querySelector('.js-menu-pin').querySelector('i').classList.add('p-icon--pin');
-    }
-    document.activeElement.blur();
-  });
+  if (menuPin) {
+    menuPin.addEventListener('click', function () {
+      navigation.classList.toggle('is-pinned');
+      if (navigation.classList.contains('is-pinned')) {
+        menuPin.querySelector('i').classList.remove('p-icon--pin');
+      } else {
+        menuPin.querySelector('i').classList.add('p-icon--pin');
+      }
+      document.activeElement.blur();
+    });
+  }
+
+  if (asidePin) {
+    asidePin.addEventListener('click', function () {
+      aside.classList.toggle('is-pinned');
+      if (aside.classList.contains('is-pinned')) {
+        asidePin.innerText = 'Unpin';
+      } else {
+        asidePin.innerText = 'Pin';
+      }
+    });
+  }
 }
 
-if (document.querySelector('.js-aside-pin')) {
-  document.querySelector('.js-aside-pin').addEventListener('click', function () {
-    document.querySelector('.l-aside').classList.toggle('is-pinned');
-    if (document.querySelector('.l-aside').classList.contains('is-pinned')) {
-      document.querySelector('.js-aside-pin').innerText = 'Unpin';
-    } else {
-      document.querySelector('.js-aside-pin').innerText = 'Pin';
-    }
-  });
-}
+setupAppLayoutExamples();
