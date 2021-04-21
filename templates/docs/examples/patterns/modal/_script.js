dde3cc12e41a807817a9fd8703da914f2e9d5ecf
Bartek Szopka <bartek.szopka@canonical.com>
2021-04-21 11:56:35 +0200

    Close modals on Escape

--- templates/docs/examples/patterns/modal/_script.js
+++ templates/docs/examples/patterns/modal/_script.js
@@ -13,10 +13,29 @@ function toggleModal(modal) {
   }
 }
 
+// Find and hide all modals on the page
+function closeModals() {
+  var modals = [].slice.apply(document.querySelectorAll('.p-modal'));
+  modals.forEach(function (modal) {
+    modal.style.display = 'none';
+  });
+}
+
 // Add click handler for clicks on elements with aria-controls
 document.addEventListener('click', function (event) {
   var targetControls = event.target.getAttribute('aria-controls');
   if (targetControls) {
     toggleModal(document.getElementById(targetControls));
+  }
+});
+
+// Add handler for closing modals using ESC key.
+document.addEventListener('keydown', function (e) {
+  e = e || window.event;
+
+  if (e.code === 'Escape') {
+    closeModals();
+  } else if (e.keyCode === 27) {
+    closeModals();
   }
 });
