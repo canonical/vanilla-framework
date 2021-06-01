f564482a9b7e88478ce42d70d64ac5c773429668
Scott Mason Nash <sowasred2012@gmail.com>
2021-06-01 14:40:08 +0100

    share subnav script between deprecated and new examples

--- templates/docs/examples/patterns/navigation/_script.js
+++ templates/docs/examples/patterns/navigation/_script.js
@@ -1,61 +1,55 @@
-(function () {
-  function toggleDropdown(toggle, open) {
-    var parentElement = toggle.parentNode;
-    var dropdown = document.getElementById(toggle.getAttribute('aria-controls'));
-    dropdown.setAttribute('aria-hidden', !open);
+function toggleDropdown(toggle, open) {
+  var parentElement = toggle.parentNode;
+  var dropdown = document.getElementById(toggle.getAttribute('aria-controls'));
+  dropdown.setAttribute('aria-hidden', !open);
 
-    if (open) {
-      parentElement.classList.add('is-active');
-    } else {
-      parentElement.classList.remove('is-active');
-    }
+  if (open) {
+    parentElement.classList.add('is-active');
+  } else {
+    parentElement.classList.remove('is-active');
   }
+}
 
-  function closeAllDropdowns(toggles) {
-    toggles.forEach(function (toggle) {
-      toggleDropdown(toggle, false);
-    });
-  }
+function closeAllDropdowns(toggles) {
+  toggles.forEach(function (toggle) {
+    toggleDropdown(toggle, false);
+  });
+}
 
-  function handleClickOutside(toggles, containerClass) {
-    document.addEventListener('click', function (event) {
-      var target = event.target;
+function handleClickOutside(toggles, containerClass) {
+  document.addEventListener('click', function (event) {
+    var target = event.target;
 
-      if (target.closest) {
-        if (!target.closest(containerClass)) {
-          closeAllDropdowns(toggles);
-        }
-      } else if (target.msMatchesSelector) {
-        // IE friendly `Element.closest` equivalent
-        // as in https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
-        do {
-          if (target.msMatchesSelector(containerClass)) {
-            return;
-          }
-          target = target.parentElement || target.parentNode;
-        } while (target !== null && target.nodeType === 1);
-
+    if (target.closest) {
+      if (!target.closest(containerClass)) {
         closeAllDropdowns(toggles);
       }
+    } else if (target.msMatchesSelector) {
+      // IE friendly `Element.closest` equivalent
+      // as in https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+      do {
+        if (target.msMatchesSelector(containerClass)) {
+          return;
+        }
+        target = target.parentElement || target.parentNode;
+      } while (target !== null && target.nodeType === 1);
+
+      closeAllDropdowns(toggles);
+    }
+  });
+}
+
+function initNavDropdowns(containerClass) {
+  var toggles = [].slice.call(document.querySelectorAll(containerClass + ' [aria-controls]'));
+
+  handleClickOutside(toggles, containerClass);
+
+  toggles.forEach(function (toggle) {
+    toggle.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      closeAllDropdowns(toggles);
+      toggleDropdown(toggle, true);
     });
-  }
-
-  function initNavDropdowns(containerClass) {
-    var toggles = [].slice.call(document.querySelectorAll(containerClass + ' [aria-controls]'));
-
-    handleClickOutside(toggles, containerClass);
-
-    toggles.forEach(function (toggle) {
-      toggle.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        closeAllDropdowns(toggles);
-        toggleDropdown(toggle, true);
-      });
-    });
-  }
-
-  var containerClass = '.p-navigation__item--dropdown-toggle';
-
-  initNavDropdowns(containerClass);
-})();
+  });
+}
