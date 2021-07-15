4c47120a3e1cb44f3a0d05523d3e5d91ece6022b
Scott Mason Nash <sowasred2012@gmail.com>
2021-07-15 11:48:50 +0100

    increase parker file size limit

--- tests/parker.js
+++ tests/parker.js
@@ -12,7 +12,7 @@ function generateMetrics(file, metricsArray) {
     {
       name: 'Stylesheet size',
       benchmark: 150000,
-      threshold: 300000,
+      threshold: 310000,
       result: results['total-stylesheet-size'],
     },
     {
