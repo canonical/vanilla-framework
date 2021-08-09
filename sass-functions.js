150b4fffb2ee8b2332a71607ea8c3c6a04bb05b3
Scott Mason Nash <sowasred2012@gmail.com>
2021-08-09 14:44:24 +0100

    remove sass-functions js, not supported by dart sass

--- sass-functions.js
+++
@@ -1,8 +0,0 @@
-var sass = require('node-sass');
-var version = require('./package.json').version;
-
-module.exports = {
-  'vanilla-version': function () {
-    return sass.types.String(version);
-  },
-};
