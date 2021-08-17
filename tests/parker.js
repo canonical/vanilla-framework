48dc8772838b694e18a76881b05d6b4b6fd68638
Scott Mason Nash <sowasred2012@gmail.com>
2021-08-17 11:18:51 +0100

    bump parker filesize threshold

--- tests/parker.js
+++ tests/parker.js
@@ -12,7 +12,7 @@ function generateMetrics(file, metricsArray) {
     {
       name: 'Stylesheet size',
       benchmark: 150000,
-      threshold: 310000,
+      threshold: 320000,
       result: results['total-stylesheet-size'],
     },
     {
