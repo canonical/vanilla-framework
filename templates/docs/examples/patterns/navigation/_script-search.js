e6df3562530bfb5b8bf30fa10b4013695c13ebb9
Bartek Szopka <bartek.szopka@canonical.com>
2021-10-15 14:07:52 +0200

    Clean up and reorder navigation search styling

--- templates/docs/examples/patterns/navigation/_script-search.js
+++ templates/docs/examples/patterns/navigation/_script-search.js
@@ -1,30 +1,20 @@
 /** @param {HTMLElement} element The tab that acts as the handles. */
 
 function initNavigationSearch(element) {
-  var searchButton = element.querySelector('.js-search-button');
-  var searchReset = element.querySelector('.p-search-box__reset');
-
-  const overlay = document.createElement('div');
-  overlay.classList.add('p-navigation-overlay');
-  overlay.classList.add('u-hide');
-  overlay.setAttribute('aria-hidden', 'true');
-  overlay.addEventListener('click', closeSearch);
-  element.appendChild(overlay);
-
+  const searchButton = element.querySelector('.js-search-button');
   if (searchButton) {
     searchButton.addEventListener('click', openSearch);
   }
 
-  if (searchReset) {
-    searchReset.addEventListener('click', closeSearch);
+  const overlay = element.querySelector('.p-navigation__search-overlay');
+  if (overlay) {
+    overlay.addEventListener('click', closeSearch);
   }
 
   function openSearch(e) {
     e.preventDefault();
     var navigation = e.target.closest('.p-navigation');
-    var curtain = document.querySelector('.p-navigation-overlay');
     var searchInput = navigation.querySelector('.p-search-box__input');
-    curtain.classList.remove('u-hide');
     navigation.classList.add('has-search-open');
     searchInput.focus();
     document.addEventListener('keyup', keyPressHandler);
@@ -32,9 +22,7 @@ function initNavigationSearch(element) {
 
   function closeSearch() {
     var navigation = document.querySelector('.p-navigation.has-search-open');
-    var curtain = document.querySelector('.p-navigation-overlay');
     var banner = document.querySelector('.p-navigation__banner');
-    curtain.classList.add('u-hide');
     navigation.classList.remove('has-search-open');
     document.removeEventListener('keyup', keyPressHandler);
   }
@@ -45,5 +33,6 @@ function initNavigationSearch(element) {
     }
   }
 }
+
 var navigation = document.querySelector('#navigation');
 initNavigationSearch(navigation);
