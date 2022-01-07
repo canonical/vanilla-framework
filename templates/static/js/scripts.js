8f2cb75248d1160e3480238a13404044222e760f
Beth Collins <bethcollinsliv92@gmail.com>
2022-01-07 16:47:03 +0000

    Add expanding search javascript to script.js

--- templates/static/js/scripts.js
+++ templates/static/js/scripts.js
@@ -79,8 +79,10 @@
       }, 200)
     );
   }
-
-  setupSideNavigation(document.querySelector('.p-side-navigation'));
+  const sideNav = document.querySelector('.p-side-navigation');
+  if (sideNav) {
+    setupSideNavigation(document.querySelector('.p-side-navigation'));
+  }
 })();
 
 // Add table of contents to side navigation on documentation pages
@@ -144,4 +146,42 @@
       e.preventDefault();
     });
   }
+})();
+
+(function () {
+  function initNavigationSearch(element) {
+    const searchButton = element.querySelector('.js-search-button');
+    if (searchButton) {
+      searchButton.addEventListener('click', openSearch);
+    }
+
+    const overlay = element.querySelector('.p-navigation__search-overlay');
+    if (overlay) {
+      overlay.addEventListener('click', closeSearch);
+    }
+
+    function openSearch(e) {
+      e.preventDefault();
+      var navigation = e.target.closest('.p-navigation');
+      var searchInput = navigation.querySelector('.p-search-box__input');
+      navigation.classList.add('has-search-open');
+      searchInput.focus();
+      document.addEventListener('keyup', keyPressHandler);
+    }
+
+    function closeSearch() {
+      var navigation = document.querySelector('.p-navigation.has-search-open');
+      var banner = document.querySelector('.p-navigation__banner');
+      navigation.classList.remove('has-search-open');
+      document.removeEventListener('keyup', keyPressHandler);
+    }
+
+    function keyPressHandler(e) {
+      if (e.key === 'Escape') {
+        closeSearch();
+      }
+    }
+  }
+  var navigation = document.querySelector('#navigation');
+  initNavigationSearch(navigation);
 })();
