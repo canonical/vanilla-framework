d73dc0650adc1cf6d90c26c83a4adf917bbf4e7a
Ilayda Cavusoglu <ilayda.cavusoglu@canonical.com>
2022-01-28 17:12:04 +0300

    Fixed lint errors

--- templates/docs/examples/patterns/buttons/_script.js
+++ templates/docs/examples/patterns/buttons/_script.js
@@ -1,14 +1,14 @@
 (function () {
-  const button = document.querySelector(".p-button--positive");
-  const hideClass = "u-hide";
-  const processingClass = "is-processing";
-  const spinner = document.querySelector(".p-icon--spinner");
-  const buttonLabel = document.getElementById("button-label");
+  const button = document.querySelector('.p-button--positive');
+  const hideClass = 'u-hide';
+  const processingClass = 'is-processing';
+  const spinner = document.querySelector('.p-icon--spinner');
+  const buttonLabel = document.getElementById('button-label');
   const buttonRect = button.getBoundingClientRect();
 
   button.addEventListener('click', function () {
-    button.style.width = buttonRect.width + "px";
-    button.style.height = buttonRect.height + "px";
+    button.style.width = buttonRect.width + 'px';
+    button.style.height = buttonRect.height + 'px';
     button.disabled = true;
 
     button.classList.add(processingClass);
