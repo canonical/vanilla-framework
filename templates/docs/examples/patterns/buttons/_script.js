c99fc2bf7dcdaebbdf2cdb89bbad8823951324f0
Ilayda Cavusoglu <ilayda.cavusoglu@canonical.com>
2022-01-28 16:52:57 +0300

    Removed processing state to normal state transition

--- templates/docs/examples/patterns/buttons/_script.js
+++ templates/docs/examples/patterns/buttons/_script.js
@@ -1,26 +1,18 @@
 (function () {
   const button = document.querySelector(".p-button--positive");
+  const hideClass = "u-hide";
+  const processingClass = "is-processing";
+  const spinner = document.querySelector(".p-icon--spinner");
+  const buttonLabel = document.getElementById("button-label");
+  const buttonRect = button.getBoundingClientRect();
 
   button.addEventListener('click', function () {
-    const hideClass = "u-hide";
-    const processingClass = "is-processing";
-    const spinner = document.querySelector(".p-icon--spinner");
-    const buttonLabel = document.getElementById("button-label");
+    button.style.width = buttonRect.width + "px";
+    button.style.height = buttonRect.height + "px";
+    button.disabled = true;
 
-    if (!spinner.classList.contains(hideClass)) {
-      spinner.classList.add(hideClass);
-      buttonLabel.classList.remove(hideClass);
-      button.classList.remove(processingClass);
-      button.disabled = false;
-    } else { 
-      const buttonRect = button.getBoundingClientRect();
-      button.style.width = buttonRect.width + "px";
-      button.style.height = buttonRect.height + "px";
-
-      button.classList.add(processingClass);
-      spinner.classList.remove(hideClass);
-      buttonLabel.classList.add(hideClass);
-      button.disabled = true;
-    }
+    button.classList.add(processingClass);
+    spinner.classList.remove(hideClass);
+    buttonLabel.classList.add(hideClass);
   });
 })();
