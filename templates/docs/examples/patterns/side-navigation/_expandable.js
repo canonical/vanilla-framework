bb9e5adb8b1a47eec4ed124fb94b8ff5f2204f84
Beth Collins <bethcollinsliv92@gmail.com>
2022-02-02 13:21:49 +0000

    Add comments explaining functionality

--- templates/docs/examples/patterns/side-navigation/_expandable.js
+++ templates/docs/examples/patterns/side-navigation/_expandable.js
@@ -1,12 +1,12 @@
 function expandCollapseSideNav() {
   // Grab the navigation
   const sideNavWrapper = document.querySelector('.p-side-navigation__drawer');
-  const sideNavDirectChildren = sideNavWrapper.children;
-  const arrayOfChildren = Array.from(sideNavDirectChildren);
+  const sideNavDirectChildren = Array.from(sideNavWrapper.children);
 
-  // Find the top level ul elements which contain the nav. No need to add chevrons to these
-  const topLevelLists = arrayOfChildren.filter((child) => child.classList.contains('p-side-navigation__list'));
+  // Find the top level ul wrappers which don't need chevrons
+  const topLevelLists = sideNavDirectChildren.filter((child) => child.classList.contains('p-side-navigation__list'));
 
+  // Function to pass first and second level nav elements to adding onclick functionality and chevron classes
   function findNavItemsToExpandCollapse(navListItem) {
     navListItem.forEach((child) => {
       const nestedList = child.childNodes.item(3);
@@ -24,6 +24,7 @@ function expandCollapseSideNav() {
 
   topLevelLists.forEach((topLevelList) => {
     const firstLevelNavItems = Array.from(topLevelList.children);
+
     // Add chevrons and on click functionality to first level lists
     findNavItemsToExpandCollapse(firstLevelNavItems);
 
@@ -32,6 +33,7 @@ function expandCollapseSideNav() {
     firstLevelListTitles.forEach((firstLevelListTitle) => {
       const secondLevelNav = Array.from(firstLevelListTitle.children);
       const secondLevelNavItems = Array.from(secondLevelNav[1].childNodes);
+
       // Add chevrons and on click functionality to second level lists
       findNavItemsToExpandCollapse(secondLevelNavItems);
     });
