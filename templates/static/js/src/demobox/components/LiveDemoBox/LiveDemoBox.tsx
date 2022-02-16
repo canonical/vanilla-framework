9e68d503f05be7895c347ac541bc679633c608f5
Beth Collins <bethcollinsliv92@gmail.com>
2022-02-16 10:40:28 +0000

    Build Live demo box component

--- templates/static/js/src/demobox/components/LiveDemoBox/LiveDemoBox.tsx
+++ templates/static/js/src/demobox/components/LiveDemoBox/LiveDemoBox.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Select from './Select';
 
 const LiveDemoBox = () => {
+  const [configValues, setConfigValues] = useState<any>({
+    dropdown: {},
+    switch: {},
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/docs/examples/patterns/notifications/toast.json');
+        const json = await response.json();
+        setConfigValues({dropdown: json.dropdown, switch: json.switch});
+      } catch (error) {
+        console.log('error', error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <section className="p-strip--light">
       <h3>Hello, I am the live demo box rendered in React!</h3>
+      {configValues && configValues.dropdown && (
+        <form>
+          <div className="row">
+            <div className="col-3">
+              <Select dropdownLabel="Type" dropdownOptions={configValues.dropdown.type} disabled={false} />
+            </div>
+            <div className="col-3">
+              <Select dropdownLabel="Theme" dropdownOptions={configValues.dropdown.theme} disabled={true} />
+            </div>
+            <div className="col-3">
+              <Select dropdownLabel="Style" dropdownOptions={configValues.dropdown.style} disabled={false} />
+            </div>
+          </div>
+          <div></div>
+        </form>
+      )}
     </section>
   );
 };
