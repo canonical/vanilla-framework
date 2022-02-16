5c904fe8df908e409a9101a7e48918f0a64ca772
minkyngkm <min.kim@canonical.com>
2022-02-16 12:20:46 +0000

    Build switch component

--- templates/static/js/src/demobox/components/LiveDemoBox/Switch.tsx
+++ templates/static/js/src/demobox/components/LiveDemoBox/Switch.tsx
@@ -5,11 +5,16 @@ type Props = {
 };
 
 const Switch = ({switchOptions}: Props) => {
-  console.log(typeof switchOptions);
   return (
     <>
-      <p>{switchOptions && switchOptions[0] && switchOptions[0].label && switchOptions[0].label}</p>
-      {switchOptions && switchOptions.map((switch: any) => <input>{switch.label}</input>)}
+      {switchOptions && switchOptions.length && switchOptions.map((option: any) => { 
+        return (
+        <label className="p-switch">
+          <input type="checkbox" class="p-switch__input" role="switch"></input>
+          <span className="p-switch__slider"></span>
+          <span className="p-switch__label">{option.label && option.label}</span>
+        </label>
+      )})}
     </>
   );
 };
