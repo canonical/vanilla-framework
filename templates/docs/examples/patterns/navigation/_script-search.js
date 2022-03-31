2805b92f97ef922094c4888b0966ff2ee1c732a7
Bartek Szopka <bartek.szopka@canonical.com>
2022-03-31 09:20:41 +0200

    Initial implementation of new mobile search menu

--- templates/docs/examples/patterns/navigation/_script-search.js
+++ templates/docs/examples/patterns/navigation/_script-search.js
@@ -1,26 +1,55 @@
 function initNavigationSearch(element) {
-  const searchButton = element.querySelector('.js-search-button');
-  if (searchButton) {
-    searchButton.addEventListener('click', openSearch);
-  }
+  const searchButtons = element.querySelectorAll('.js-search-button');
+
+  searchButtons.forEach((searchButton) => {
+    searchButton.addEventListener('click', toggleSearch);
+  });
 
   const overlay = element.querySelector('.p-navigation__search-overlay');
   if (overlay) {
     overlay.addEventListener('click', closeSearch);
   }
 
+  function toggleSearch(e) {
+    e.preventDefault();
+    const button = e.target.closest('.js-search-button');
+
+    var navigation = e.target.closest('.p-navigation');
+    if (navigation.classList.contains('has-search-open')) {
+      closeSearch(e);
+    } else {
+      openSearch(e);
+    }
+  }
+
   function openSearch(e) {
     e.preventDefault();
     var navigation = e.target.closest('.p-navigation');
+    var nav = navigation.querySelector('.p-navigation__nav');
+
     var searchInput = navigation.querySelector('.p-search-box__input');
-    navigation.classList.add('has-search-open');
+    var buttons = document.querySelectorAll('.js-search-button');
+
+    searchButtons.forEach((searchButton) => {
+      searchButton.setAttribute('aria-pressed', true);
+    });
+
+    navigation.classList.add('has-search-open'); // for legacy expanding search
     searchInput.focus();
     document.addEventListener('keyup', keyPressHandler);
   }
 
   function closeSearch() {
     var navigation = document.querySelector('.p-navigation.has-search-open');
+    var nav = navigation.querySelector('.p-navigation__nav');
+
     var banner = document.querySelector('.p-navigation__banner');
+    var buttons = document.querySelectorAll('.js-search-button');
+
+    searchButtons.forEach((searchButton) => {
+      searchButton.removeAttribute('aria-pressed');
+    });
+
     navigation.classList.remove('has-search-open');
     document.removeEventListener('keyup', keyPressHandler);
   }
