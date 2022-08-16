6777bf83ebbcdfbc7955433906e2d845258f847f
Peter Makowski <peter@makowski.uk>
2022-08-16 16:55:34 +0200

    add _expand_script.js

---
+++ templates/docs/examples/patterns/side-navigation/_expand_script.js
@@ -0,0 +1,36 @@
+// Setup expand toggles for all side navigations on the page.
+var expandToggles = document.querySelectorAll('.p-side-navigation__expand');
+var navigationLinks = document.querySelectorAll('.p-side-navigation__link');
+
+const toggleItem = (item) => {
+  if (item.getAttribute('aria-expanded') === 'true') {
+    if (item.querySelector('.p-side-navigation__list').className.includes('fade-in--after')) {
+      item.querySelector('.p-side-navigation__list').classList.remove('fade-in--after');
+      setTimeout(() => item.setAttribute('aria-expanded', false), 200);
+    }
+  } else {
+    item.setAttribute('aria-expanded', true);
+    if (item.querySelector('.p-side-navigation__list').className.includes('fade-in')) {
+      requestAnimationFrame(() => item.querySelector('.p-side-navigation__list').classList.add('fade-in--after'));
+    }
+  }
+};
+
+navigationLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    const item = e.currentTarget.closest('.p-side-navigation__item');
+    const isExpandable = item.hasAttribute('aria-expanded');
+    console.log(item, isExpandable);
+    const expanded = item.getAttribute('aria-expanded') === true;
+    if (isExpandable) {
+      toggleItem(item);
+    }
+  });
+});
+
+expandToggles.forEach((toggle) => {
+  toggle.addEventListener('click', () => {
+    const item = toggle.closest('.p-side-navigation__item');
+    toggleItem(item);
+  });
+});
