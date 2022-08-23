f44ef7e3fb2e5b9c8c26e27d675c02ed72ef5b0b
Peter Makowski <peter@makowski.uk>
2022-08-23 10:25:14 +0200

    accordion content implies navigation #4547

--- cypress/e2e/patterns/accordion.cy.js
+++ cypress/e2e/patterns/accordion.cy.js
@@ -4,12 +4,12 @@ describe('accordion', () => {
   });
   it('opens and closes a single list item on click', () => {
     cy.findByRole('list').should('be.visible');
-    cy.findByRole('region', {name: /Networking/}).should('not.exist');
-    cy.findByRole('button', {name: /Networking/}).click();
-    cy.findByRole('region', {name: /Networking/}).should('exist');
+    cy.findByRole('region', {name: /What MAAS offers/}).should('not.exist');
+    cy.findByRole('button', {name: /What MAAS offers/}).click();
+    cy.findByRole('region', {name: /What MAAS offers/}).should('exist');
     cy.findAllByRole('region').should('have.length', 1);
-    cy.findByRole('button', {name: /Networking/}).click();
-    cy.findByRole('region', {name: /Networking/}).should('not.exist');
+    cy.findByRole('button', {name: /What MAAS offers/}).click();
+    cy.findByRole('region', {name: /What MAAS offers/}).should('not.exist');
     cy.findAllByRole('region').should('have.length', 0);
   });
 });
