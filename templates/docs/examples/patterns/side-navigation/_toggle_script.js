758dc9307f888ba3d7a28cbe570cf419405a84db
Bartek Szopka <bartek.szopka@canonical.com>
2022-09-09 09:22:27 +0200

    Clean up side navigation scripts

--- templates/docs/examples/patterns/side-navigation/_toggle_script.js
+++ templates/docs/examples/patterns/side-navigation/_toggle_script.js
@@ -103,36 +103,3 @@ function setupSideNavigations(sideNavigationSelector) {
 }
 
 setupSideNavigations('.p-side-navigation, [class*="p-side-navigation--"]');
-
-var expandToggles = document.querySelectorAll('.p-side-navigation__accordion-button');
-var navigationLinks = document.querySelectorAll('.p-side-navigation__link');
-
-const setup = (toggle) => {
-  const isExpanded = toggle.getAttribute('aria-expanded') === 'true';
-  if (!isExpanded) {
-    toggle.setAttribute('aria-expanded', isExpanded);
-  }
-  const item = toggle.closest('.p-side-navigation__item');
-  const link = item.querySelector('.p-side-navigation__link');
-  const nestedList = item.querySelector('.p-side-navigation__list');
-  if (!link?.hasAttribute('aria-expanded')) {
-    link.setAttribute('aria-expanded', isExpanded);
-  }
-  if (!nestedList?.hasAttribute('aria-expanded')) {
-    nestedList.setAttribute('aria-expanded', isExpanded);
-  }
-};
-
-const handleToggle = (e) => {
-  const item = e.currentTarget.closest('.p-side-navigation__item');
-  const button = item.querySelector('.p-side-navigation__accordion-button');
-  const nestedList = item.querySelector('.p-side-navigation__list');
-  [button, nestedList].forEach((el) => el.setAttribute('aria-expanded', el.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'));
-};
-
-expandToggles.forEach((toggle) => {
-  setup(toggle);
-  toggle.addEventListener('click', (e) => {
-    handleToggle(e);
-  });
-});
