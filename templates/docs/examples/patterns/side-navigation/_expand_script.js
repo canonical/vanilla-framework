a4aa9776f7cc46a948b65860187114a6f4b5423a
Bartek Szopka <bartek.szopka@canonical.com>
2022-09-09 09:22:27 +0200

    Clean up side navigation scripts

--- templates/docs/examples/patterns/side-navigation/_expand_script.js
+++ templates/docs/examples/patterns/side-navigation/_expand_script.js
@@ -1,9 +1,6 @@
 // Setup expand toggles for all side navigations on the page.
 
-var expandToggles = document.querySelectorAll('.p-side-navigation__expand');
-var navigationLinks = document.querySelectorAll('.p-side-navigation__link');
-
-const setup = (toggle) => {
+function setupSideNavigationExpandToggle(toggle) {
   const isExpanded = toggle.getAttribute('aria-expanded') === 'true';
   if (!isExpanded) {
     toggle.setAttribute('aria-expanded', isExpanded);
@@ -17,19 +14,27 @@ const setup = (toggle) => {
   if (!nestedList?.hasAttribute('aria-expanded')) {
     nestedList.setAttribute('aria-expanded', isExpanded);
   }
-};
+}
 
-const handleToggle = (e) => {
-  const item = e.currentTarget.closest('.p-side-navigation__item');
-  const button = item.querySelector('.p-side-navigation__expand');
+function handleExpandToggle(event) {
+  const item = event.currentTarget.closest('.p-side-navigation__item');
+  const button = item.querySelector('.p-side-navigation__expand, .p-side-navigation__accordion-button');
   const link = item.querySelector('.p-side-navigation__link');
   const nestedList = item.querySelector('.p-side-navigation__list');
-  [button, link, nestedList].forEach((el) => el.setAttribute('aria-expanded', el.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'));
-};
 
-expandToggles.forEach((toggle) => {
-  setup(toggle);
-  toggle.addEventListener('click', (e) => {
-    handleToggle(e);
+  [button, link, nestedList].forEach((el) => {
+    el.setAttribute('aria-expanded', el.getAttribute('aria-expanded') === 'true' ? 'false' : 'true');
   });
-});
+}
+
+function setupSideNavigationExpands() {
+  var expandToggles = document.querySelectorAll('.p-side-navigation__expand, .p-side-navigation__accordion-button');
+  expandToggles.forEach((toggle) => {
+    setupSideNavigationExpandToggle(toggle);
+    toggle.addEventListener('click', (e) => {
+      handleExpandToggle(e);
+    });
+  });
+}
+
+setupSideNavigationExpands();
