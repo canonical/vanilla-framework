b47da760a5604bd7c989e66781f0103063630667
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2023-03-09 17:18:00 +0530

    auto expand if page in view

--- templates/static/js/scripts.js
+++ templates/static/js/scripts.js
@@ -215,6 +215,51 @@
       parent.appendChild(list);
     }
   }
+  
+  //
+  var currentPage = document.querySelector('.p-side-navigation__link[aria-current="page"]');
+  var parentList = currentPage.parentNode.parentNode;
+  parentList.setAttribute("aria-expanded",true);
+  parentList.previousElementSibling.setAttribute("aria-expanded",true);
+
+  function setupSideNavigationExpandToggle(toggle) {
+    const isExpanded = toggle.getAttribute('aria-expanded') === 'true';
+    if (!isExpanded) {
+      toggle.setAttribute('aria-expanded', isExpanded);
+    }
+    const item = toggle.closest('.p-side-navigation__item');
+    const link = item.querySelector('.p-side-navigation__link');
+    const nestedList = item.querySelector('.p-side-navigation__list');
+    if (!link?.hasAttribute('aria-expanded')) {
+      link.setAttribute('aria-expanded', isExpanded);
+    }
+    if (!nestedList?.hasAttribute('aria-expanded')) {
+      nestedList.setAttribute('aria-expanded', isExpanded);
+    }
+  }
+  
+  function handleExpandToggle(event) {
+    const item = event.currentTarget.closest('.p-side-navigation__item');
+    const button = item.querySelector('.p-side-navigation__expand, .p-side-navigation__accordion-button');
+    const link = item.querySelector('.p-side-navigation__link');
+    const nestedList = item.querySelector('.p-side-navigation__list');
+  
+    [button, link, nestedList].forEach((el) => {
+      el.setAttribute('aria-expanded', el.getAttribute('aria-expanded') === 'true' ? 'false' : 'true');
+    });
+  }
+  
+  function setupSideNavigationExpands() {
+    var expandToggles = document.querySelectorAll('.p-side-navigation__expand, .p-side-navigation__accordion-button');
+    expandToggles.forEach((toggle) => {
+      setupSideNavigationExpandToggle(toggle);
+      toggle.addEventListener('click', (e) => {
+        handleExpandToggle(e);
+      });
+    });
+  }
+  
+  setupSideNavigationExpands();
 })();
 
 // scroll active side navigation item into view (without scrolling whole page)
