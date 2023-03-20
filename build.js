54ff0ef497a184dee226d1ff56ac5842e6e23420
ClementChaumel <clement.chaumel@gmail.com>
2023-03-20 19:02:06 +0100

    Dynamically builds every _script file instead

--- build.js
+++ build.js
@@ -1,24 +1,40 @@
 const {build} = require('esbuild');
-
-const entryFiles = 'js/index.ts';
+const fs = require('fs');
+const path = require('path');
+const patternsDir = 'templates/docs/examples/patterns';
 
 const buildOptions = {
   bundle: true,
-  entryPoints: [entryFiles],
   logLevel: 'info',
   minify: true,
   sourcemap: true,
+  outdir: './build/js/out/',
   target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
 };
 
-build({
-  ...buildOptions,
-  format: 'esm',
-  outfile: './build/js/index.esm.js',
-});
+// for every subdirectory in the examples/patterns directory if there is a _script.js file, build it
+fs.readdirSync(patternsDir).forEach((dir) => {
+  const scriptPath = path.join(patternsDir, dir, '_script.js');
+  // test if the scriptPath exists
+  fs.access(scriptPath, fs.constants.F_OK, (err) => {
+    if (err) {
+      return;
+    } else {
+      const dirName = path.basename(path.dirname(scriptPath));
 
-build({
-  ...buildOptions,
-  format: 'cjs',
-  outfile: './build/js/index.cjs.js',
+      build({
+        ...buildOptions,
+        format: 'esm',
+        entryPoints: [scriptPath],
+        entryNames: dirName,
+      });
+
+      build({
+        ...buildOptions,
+        format: 'cjs',
+        entryPoints: [scriptPath],
+        entryNames: `${dirName}-cjs`,
+      });
+    }
+  });
 });
