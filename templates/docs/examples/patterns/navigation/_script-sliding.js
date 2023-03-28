9e52d6868f7eb544b953090b90e935086b5421b0
Ogheneochuko Pedro <pedroogheneochuko@gmail.com>
2023-03-28 09:00:33 +0100

    feat(nav) WD-2617 clean-up navigation code

--- templates/docs/examples/patterns/navigation/_script-sliding.js
+++ templates/docs/examples/patterns/navigation/_script-sliding.js
@@ -1,13 +1,21 @@
 function toggleDropdown(toggle, open) {
-  const parentElement = toggle.parentNode;
-  // const parentSlide = document.getElementById(parentElement.getAttribute('data-parent'));
+  let parentElement;
+  if (toggle.parentNode.classList.contains('is-back')) {
+    parentElement = toggle.parentNode.parentNode.parentNode;
+  } else {
+    parentElement = toggle.parentNode;
+  }
+
   const dropdown = document.getElementById(toggle.getAttribute('aria-controls'));
+
   dropdown.setAttribute('aria-hidden', !open);
 
   if (open) {
     parentElement.classList.add('is-active');
+    dropdown.removeAttribute('inert');
   } else {
     parentElement.classList.remove('is-active');
+    dropdown.setAttribute('inert', true);
   }
 }
 
@@ -15,7 +23,7 @@ function closeAllDropdowns(toggles, exceptions) {
   toggles.forEach(function (toggle) {
     if (exceptions) {
       // if the dropdown is in one of the exceptions, skip it
-      if (!exceptions[toggle.parentNode.id]) {
+      if (!exceptions[toggle.parentNode.id] && !toggle.parentNode.classList.contains('is-back')) {
         toggleDropdown(toggle, false);
       }
     } else {
@@ -42,30 +50,39 @@ function initNavDropdowns(containerClass) {
   handleClickOutside(toggles, containerClass);
 
   toggles.forEach(function (toggle) {
+    // initialise all dropdowns to inert when the page loads
+    if (toggle.parentNode.classList.contains('is-back')) {
+      toggle.parentNode.parentNode.setAttribute('inert', true);
+    }
+
     toggle.addEventListener('click', async function (e) {
       e.preventDefault();
 
-      // get all active toggle parents of the clicked toggle
-      let element;
+      let shouldOpen;
 
-      if (toggle.parentNode.classList.contains('is-back')) {
-        element = document.getElementById(toggle.getAttribute('aria-controls')).parentNode; // if the clicked toggle is a back button, get the parent of the dropdown
-      } else {
-        element = toggle.parentNode;
-      }
-      const classNames = [containerClass.split('.')[1], 'is-active'];
-      const parents = {};
+      if (!toggle.parentNode.classList.contains('is-back')) {
+        const parents = {};
+        let element = toggle.parentNode;
 
-      while (element.parentNode && element.parentNode.nodeName.toLowerCase() != 'body') {
-        element = element.parentNode;
+        const classNames = [containerClass.split('.')[1], 'is-active'];
 
-        if (classNames.every((className) => element.classList.contains(className))) {
-          parents[element.id] = element;
+        // get all parents that are active, and exclude them from being closed when a new dropdown is opened
+        while (element.parentNode && element.parentNode.nodeName.toLowerCase() != 'body') {
+          element = element.parentNode;
+
+          if (classNames.every((className) => element.classList.contains(className))) {
+            parents[element.id] = element;
+          }
         }
+
+        shouldOpen = !toggle.parentNode.classList.contains('is-active');
+
+        // close all dropdowns except the ones that are parents of the clicked toggle
+        closeAllDropdowns(toggles, parents);
+      } else {
+        shouldOpen = false; // close the dropdown when clicking on the back button
       }
 
-      const shouldOpen = !toggle.parentNode.classList.contains('is-active');
-      closeAllDropdowns(toggles, parents);
       toggleDropdown(toggle, shouldOpen);
     });
   });
