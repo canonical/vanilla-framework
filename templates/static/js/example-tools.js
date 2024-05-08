c8403cae10f158e769330e8362a347086ca88749
Julie Muzina <julie.muzina@canonical.com>
2024-05-08 14:34:39 -0400

    request, set, & persist color theme with all JS

--- templates/static/js/example-tools.js
+++ templates/static/js/example-tools.js
@@ -1,3 +1,8 @@
+const SUPPORTED_THEMES = ['Light', 'Dark', 'Paper'];
+const [DEFAULT_COLOR_THEME] = SUPPORTED_THEMES;
+const COLOR_THEME_QUERY_PARAM_NAME = 'theme';
+var activeTheme = DEFAULT_COLOR_THEME;
+
 (function () {
   function inIframe() {
     try {
@@ -13,19 +18,110 @@
     return temp.content;
   }
 
+  /**
+   * Gets the current query parameters
+   * @returns {URLSearchParams}
+   */
+  function getQueryParameters() {
+    return new URLSearchParams(window.location.search);
+  }
+
+  /**
+   * Sets the query parameter value of `key` to `value`
+   * @param {String} key
+   * @param {String} value
+   * @param {Boolean} reload Whether to cause a page load after updating the query parameter
+   * @returns {URLSearchParams} Query parameters after update
+   */
+  function setQueryParameter(key, value, reload = false) {
+    var currentQueryParams = getQueryParameters();
+
+    if (reload && currentQueryParams.get(key) !== value) {
+      currentQueryParams.set(key, value);
+      window.location.search = currentQueryParams.toString();
+    }
+    else {
+      var url = new URL(window.location.href);
+      url.searchParams.set(key, value);
+      window.history.pushState(null, '', url.toString());
+    }
+    return getQueryParameters();
+  }
+
+  /**
+   * Converts a theme name to its document class name, used for applying that color theme to the body
+   * @param {String} themeName
+   * @returns {string}
+   */
+  function convertThemeNameToClassName(themeName) {
+    return `is-${themeName.toLowerCase()}`;
+  }
+
+  /**
+   * Converts a theme name to its JS toggler class name, used for targeting it with JS events
+   * @param {String} themeName
+   * @returns {string}
+   */
+  function convertThemeNameToJsTogglerClassName(themeName) {
+    return `js-${themeName.toLowerCase()}-theme-toggle`;
+  }
+
+  /**
+   * Converts a theme name to its toggler utility class name, used for applying style to the toggler button
+   * @param {String} themeName
+   * @returns {string}
+   */
+  function convertThemeNameToTogglerUtilityClassName(themeName) {
+    return `u-theme-toggle__${themeName.toLowerCase()}`
+  }
+
+  /**
+   * Sets a color theme as active; removes all other color themes from active status
+   * @param {String} themeToSelect
+   */
+  function selectColorTheme(themeToSelect) {
+    // Apply the color theme to the document body
+    document.body.classList.add(convertThemeNameToClassName(themeToSelect));
+    // Remove the old color theme from the document body
+    if (themeToSelect !== activeTheme) document.body.classList.remove(convertThemeNameToClassName(activeTheme));
+
+    // Update address bar to reflect the newly selected color theme
+    setQueryParameter(COLOR_THEME_QUERY_PARAM_NAME, themeToSelect.toLowerCase());
+
+    // Update theme selector button states to reflect which one is currently active
+    var themeButtonToSelect = document.querySelector(`.${convertThemeNameToJsTogglerClassName(themeToSelect)}`);
+    themeButtonToSelect?.setAttribute('aria-selected', "true");
+    var themeButtonCurrentlySelected = document.querySelector(`.${convertThemeNameToJsTogglerClassName(activeTheme)}`);
+    themeButtonCurrentlySelected?.setAttribute('aria-selected', 'false');
+
+    activeTheme = themeToSelect;
+  }
+
   if (!inIframe()) {
     document.documentElement.classList.add('u-baseline-grid');
 
     document.addEventListener('DOMContentLoaded', function () {
       var body = document.body;
-      var controls = fragmentFromString(
-        '<div class="u-baseline-grid__toggle"><label class="p-switch"><input type="checkbox" class="p-switch__input js-baseline-toggle" /><span class="p-switch__slider"></span><span class="p-switch__label">Toggle baseline grid</span></label></div>',
-      );
-      var themes = fragmentFromString(
-        '<div class="u-theme-toggle is-light"><button class="u-theme-toggle__button u-theme-toggle__dark js-dark-theme-toggle">Dark</button><button class="u-theme-toggle__button u-theme-toggle__light js-light-theme-toggle">Light</button><button class="u-theme-toggle__button u-theme-toggle__paper js-paper-theme-toggle">Paper</button></div>',
-      );
+      var requestedColorTheme = getQueryParameters().get(COLOR_THEME_QUERY_PARAM_NAME);
 
-      body.appendChild(themes);
+      // Validate requested color theme
+      if (
+        requestedColorTheme
+        && SUPPORTED_THEMES.some(supportedTheme => supportedTheme.toLowerCase() === requestedColorTheme.toLowerCase())
+      ) {
+        selectColorTheme(requestedColorTheme);
+      } else {
+        selectColorTheme(DEFAULT_COLOR_THEME);
+      }
+
+      var themeSwitcherControls = SUPPORTED_THEMES.map(themeLabel => `<button class="p-segmented-control__button u-theme-toggle__button is-dense ${convertThemeNameToTogglerUtilityClassName(themeLabel)} ${convertThemeNameToJsTogglerClassName(themeLabel)}" role="button" aria-selected="${activeTheme === themeLabel.toLowerCase()}" id="theme-selector-${themeLabel.toLowerCase()}" data-color-theme-name="${themeLabel.toLowerCase()}">${themeLabel}</button>`);
+      var themeSwitcherSegmentedControl = fragmentFromString(`<div class="p-segmented-control u-theme-toggle"><div class="p-segmented-control__list" role="list">${themeSwitcherControls.join('')}</div></div>`)
+      var baselineGridControl = fragmentFromString('<div class="u-baseline-grid__toggle"><label class="p-switch"><input type="checkbox" class="p-switch__input js-baseline-toggle" /><span class="p-switch__slider"></span><span class="p-switch__label">Toggle baseline grid</span></label></div>')
+
+      var controls = document.createElement('div');
+      controls.classList.add('u-example-controls', 'p-form', 'p-form--inline');
+      controls.appendChild(themeSwitcherSegmentedControl);
+      controls.appendChild(baselineGridControl);
       body.appendChild(controls);
 
       var toggle = document.querySelector('.js-baseline-toggle');
@@ -33,32 +129,18 @@
         body.classList.toggle('u-baseline-grid');
       });
 
-      var themeToggles = document.querySelector('.u-theme-toggle');
+      var themeToggleContainer = document.querySelector('.u-theme-toggle');
 
       if (!SHOW_THEME_SWITCH) {
-        themeToggles.classList.add('u-hide');
+        themeToggleContainer.classList.add('u-hide');
       }
 
-      var darkTheme = document.querySelector('.js-dark-theme-toggle');
-      darkTheme.addEventListener('click', function (event) {
-        body.classList.add('is-dark');
-        body.classList.remove('is-paper');
-        body.classList.remove('is-light');
-      });
-
-      var lightTheme = document.querySelector('.js-light-theme-toggle');
-      lightTheme.addEventListener('click', function (event) {
-        body.classList.add('is-light');
-        body.classList.remove('is-dark');
-        body.classList.remove('is-paper');
-      });
-
-      var paperTheme = document.querySelector('.js-paper-theme-toggle');
-      paperTheme.addEventListener('click', function (event) {
-        body.classList.add('is-paper');
-        body.classList.remove('is-dark');
-        body.classList.remove('is-light');
-      });
+      var themeToggleButtons = document.querySelectorAll('.u-theme-toggle__button');
+      themeToggleButtons.forEach(themeToggleButton => {
+        themeToggleButton.addEventListener('click', () => {
+          selectColorTheme(themeToggleButton.getAttribute('data-color-theme-name'))
+        })
+      })
     });
   }
 })();
