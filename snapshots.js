f5ce53b9b4561467696e06a70f25104c2ab47248
Julie Muzina <julie.muzina@canonical.com>
2024-05-21 12:45:20 -0400

    Revert "only use 1 snapshot for percy test"
    This reverts commit a290f351637d4892a891a0038c4e4604bae4ef81.

--- snapshots.js
+++ snapshots.js
@@ -31,7 +31,7 @@ async function getPercyConfigURLs() {
   let links = getExampleUrls(await getExampleFiles());
   let urls = [];
 
-  for (var i = 0; i < 1; i++) {
+  for (var i = 0; i < links.length; i++) {
     const url = links[i];
 
     const path = new URL(url).pathname.replace(/\/?$/, '/');
