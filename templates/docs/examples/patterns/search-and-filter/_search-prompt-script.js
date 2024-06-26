515e2fd2ce83902813c06f4cd1da2670fa4f92eb
Julie Muzina <julie.muzina@canonical.com>
2024-06-26 02:03:57 -0400

    Percy example combinations - `patterns/search-and-filter` (#5177)
    * spacing option in example template
    
    * patterns/search-and-filter
    
    * search and filter uses spacing option
    
    * spacing option in example template
    
    * fix search prompt functionality breaking comined examples (applying  search & filter functionality to search & filter components with no search prompt)
    
    * replace search and filter spacing with rems
    
    * rm combined.scss (superseded by rem spacing)
    
    * update search & filter var naming per code review
    
    * reorder search & filter combined examples

--- templates/docs/examples/patterns/search-and-filter/_search-prompt-script.js
+++ templates/docs/examples/patterns/search-and-filter/_search-prompt-script.js
@@ -12,46 +12,51 @@ function createChip(value) {
 }
 
 // Add click handler for clicks on elements with aria-controls
-[].slice.call(document.querySelectorAll('.p-search-and-filter')).forEach(function (pattern) {
-  var input = pattern.querySelector('.p-search-and-filter__input');
-  var query = pattern.querySelector('.p-search-and-filter__search-query');
-  var prompt = pattern.querySelector('.p-search-and-filter__search-prompt');
-  var container = pattern.querySelector('.p-search-and-filter__search-container');
-  var searchBox = pattern.querySelector('.p-search-and-filter__box');
-  var clear = pattern.querySelector('.p-search-and-filter__clear');
+[].slice
+  .call(document.querySelectorAll('.p-search-and-filter'))
+  .filter(function (searchAndFilterComponent) {
+    return searchAndFilterComponent.querySelector('.p-search-and-filter__search-prompt');
+  })
+  .forEach(function (searchAndFilterComponent) {
+    var input = searchAndFilterComponent.querySelector('.p-search-and-filter__input');
+    var query = searchAndFilterComponent.querySelector('.p-search-and-filter__search-query');
+    var prompt = searchAndFilterComponent.querySelector('.p-search-and-filter__search-prompt');
+    var container = searchAndFilterComponent.querySelector('.p-search-and-filter__search-container');
+    var searchBox = searchAndFilterComponent.querySelector('.p-search-and-filter__box');
+    var clear = searchAndFilterComponent.querySelector('.p-search-and-filter__clear');
 
-  clear.addEventListener('click', function (event) {
-    input.value = '';
-    clear.style.display = 'none';
-  });
-
-  input.addEventListener('keyup', function (event) {
-    query.innerHTML = input.value;
-    if (input.value === '') {
-      clear.style.display = 'none';
-    } else {
-      clear.style.display = 'block';
-    }
-  });
-
-  prompt.addEventListener('click', function (event) {
-    if (input.value !== '') {
-      container.insertBefore(createChip(input.value), searchBox);
+    clear.addEventListener('click', function (event) {
       input.value = '';
       clear.style.display = 'none';
-      query.innerHTML = '';
-    }
-  });
+    });
 
-  input.addEventListener('blur', function (event) {
-    var targetPanel = pattern.querySelector('.p-search-and-filter__panel');
-    targetPanel.setAttribute('aria-hidden', 'true');
-    container.setAttribute('aria-expanded', 'false');
-  });
+    input.addEventListener('keyup', function (event) {
+      query.innerHTML = input.value;
+      if (input.value === '') {
+        clear.style.display = 'none';
+      } else {
+        clear.style.display = 'block';
+      }
+    });
 
-  input.addEventListener('focus', function (event) {
-    var targetPanel = pattern.querySelector('.p-search-and-filter__panel');
-    targetPanel.setAttribute('aria-hidden', 'false');
-    container.setAttribute('aria-expanded', 'true');
+    prompt.addEventListener('click', function (event) {
+      if (input.value !== '') {
+        container.insertBefore(createChip(input.value), searchBox);
+        input.value = '';
+        clear.style.display = 'none';
+        query.innerHTML = '';
+      }
+    });
+
+    input.addEventListener('blur', function (event) {
+      var targetPanel = searchAndFilterComponent.querySelector('.p-search-and-filter__panel');
+      targetPanel.setAttribute('aria-hidden', 'true');
+      container.setAttribute('aria-expanded', 'false');
+    });
+
+    input.addEventListener('focus', function (event) {
+      var targetPanel = searchAndFilterComponent.querySelector('.p-search-and-filter__panel');
+      targetPanel.setAttribute('aria-hidden', 'false');
+      container.setAttribute('aria-expanded', 'true');
+    });
   });
-});
