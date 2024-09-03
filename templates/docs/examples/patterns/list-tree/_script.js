0b991f7d8ecc39ce550679432111206a1d7fe912
Julie Muzina <julie.muzina@canonical.com>
2024-09-03 15:59:55 -0400

    cleanup active selectors, fix case where clicking a subtree may highlight its toggler

--- templates/docs/examples/patterns/list-tree/_script.js
+++ templates/docs/examples/patterns/list-tree/_script.js
@@ -31,15 +31,37 @@ function setupListTreeToggle(listTreeToggle) {
  * @param {HTMLElement} listTreeLink - `.p-list-tree__toggle` or `.p-list-tree__item` element to set up.
  * @param {HTMLElement[]} otherTreeLinks - Other `.p-list-tree__toggle` or `.p-list-tree__item` elements on the page.
  */
-function setupListTreeLink(listTreeLink, otherTreeLinks) {
-  listTreeLink.addEventListener('click', function (event) {
+function setupListTreeLink(listTreeLink) {
+  // Set up a click event on the link inside the node grouping if
+  const isGroup = listTreeLink.classList.contains('p-list-tree__item--group');
+  const childLink = listTreeLink.querySelector('.p-list-tree__link');
+  const clickTarget = isGroup || !childLink ? listTreeLink : childLink;
+  clickTarget.addEventListener('click', function (event) {
     event.preventDefault();
+    // Prevent node clicks from bubbling up to their ancestor nodes
+    event.stopPropagation();
 
-    listTreeLink.classList.add('is-active');
+    // Set active state on the child toggle button if a grouping was clicked. Otherwise, set active state on the node itself.
+    const childToggleButton = clickTarget.querySelector('.p-list-tree__toggle');
 
-    otherTreeLinks.forEach((otherTreeLink) => {
-      otherTreeLink.classList.remove('is-active');
-    });
+    // Determine which element to place `.is-active` on
+    let activeTarget = clickTarget;
+    if (isGroup && childToggleButton) {
+      activeTarget = childToggleButton;
+    } else if (childLink) {
+      activeTarget = childLink;
+    }
+
+    // The highest level tree that contains this node
+    const parentTree = clickTarget.closest('.p-list-tree[role="tree"]');
+
+    // Remove active state from any other active nodes in this tree
+    const linksToDeactivate = parentTree.querySelectorAll('.is-active');
+    linksToDeactivate.forEach(linkToDeactivate => {
+      linkToDeactivate.classList.remove('is-active');
+    })
+
+    activeTarget.classList.add('is-active');
   });
 }
 
@@ -50,12 +72,10 @@ for (var i = 0, l = listTreeToggles.length; i < l; i++) {
 }
 
 // Set up all the links on the page.
-var listTreeItems = document.querySelectorAll('.p-list-tree__item:not(.p-list-tree__item--group)');
-var listTreeLinkItems = [...listTreeToggles, ...listTreeItems];
-for (var i = 0; i < listTreeLinkItems.length; i++) {
+var listTreeItems = document.querySelectorAll('li.p-list-tree__item:not(.p-list-tree__item--group)');
+var listTreeLinks = [...listTreeItems, ...listTreeToggles];
+for (var i = 0; i < listTreeLinks.length; i++) {
   setupListTreeLink(
-    listTreeLinkItems[i],
-    // all items besides this one
-    listTreeLinkItems.filter((_, j) => i !== j),
+    listTreeLinks[i]
   );
 }
