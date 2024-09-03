cc1ec444f2379d57e9976bcd1d62f4c1ef8aabc8
Julie Muzina <julie.muzina@canonical.com>
2024-09-03 14:26:17 -0400

    Move utils to window scope, rather than JS import

--- templates/static/js/shared/utils.js
+++ templates/static/js/shared/utils.js
@@ -1,38 +1,44 @@
+(function() {
 // throttling function calls, by Remy Sharp
 // http://remysharp.com/2010/07/21/throttling-function-calls/
-export const throttle = function (fn, delay) {
-  let timer = null;
-  return function () {
-    let context = this,
-      args = arguments;
-    clearTimeout(timer);
-    timer = setTimeout(function () {
-      fn.apply(context, args);
-    }, delay);
+  const throttle = function(fn, delay) {
+    let timer = null;
+    return function() {
+      let context = this,
+        args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(function() {
+        fn.apply(context, args);
+      }, delay);
+    };
   };
-};
 
-/**
- * `fetch()` wrapper that throws an error if the response is not OK.
- * @param {String} url Address to fetch
- * @param {RequestInit} opts Options for the fetch request
- * @returns {Promise<Response>} Response object
- * @throws {Error} If the response is not in the 200 (OK) range
- */
-export const fetchResponse = async function (url, opts = {}) {
-  const response = await fetch(url, opts);
-  if (!response.ok) {
-    throw new Error(`Failed to fetch example ${url} with status ${response.status}`);
+  /**
+   * `fetch()` wrapper that throws an error if the response is not OK.
+   * @param {String} url Address to fetch
+   * @param {RequestInit} opts Options for the fetch request
+   * @returns {Promise<Response>} Response object
+   * @throws {Error} If the response is not in the 200 (OK) range
+   */
+  const fetchResponse = async function(url, opts = {}) {
+    const response = await fetch(url, opts);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch example ${url} with status ${response.status}`);
+    }
+    return response;
+  };
+
+  /**
+   * Fetch the response text of a URL.
+   * @param {String} url Address to fetch
+   * @returns {Promise<String>} Response text
+   * @throws {Error} If the response is not in the 200 (OK) range
+   */
+   const fetchResponseText = async function(url) {
+    return (await fetchResponse(url)).text();
   }
-  return response;
-};
 
-/**
- * Fetch the response text of a URL.
- * @param {String} url Address to fetch
- * @returns {Promise<String>} Response text
- * @throws {Error} If the response is not in the 200 (OK) range
- */
-export const fetchResponseText = async function (url) {
-  return (await fetchResponse(url)).text();
-};
+  window.throttle = throttle;
+  window.fetchResponse = fetchResponse;
+  window.fetchResponseText = fetchResponseText;
+})();
