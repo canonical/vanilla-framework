6fccc824312ad5494f640c6191878b911ef336c7
pastelcyborg <168636120+pastelcyborg@users.noreply.github.com>
2024-09-09 17:19:49 -0400

    Clean up sd config runner

--- sd.config.mjs
+++ sd.config.mjs
@@ -1,19 +1,11 @@
 import StyleDictionary from 'style-dictionary';
-// import {dirname} from 'path';
-// import {fileURLToPath} from 'url';
 
-// const __dirname = dirname(fileURLToPath(import.meta.url));
-
-console.log('Build started...');
-console.log('\n==============================================');
-
-// REGISTER THE CUSTOM TRANSFORMS
+console.log('Style Dictionary build started...');
+console.log('==============================================');
 
 StyleDictionary.registerTransform({
-  // this is a silly example, to show how you can apply transform to names
-  name: 'name/squiggle',
+  name: 'name/theme',
   type: 'name',
-  // notice: if you don't specify a filter, the transformation will be applied to all the tokens
   transform: function (token) {
     let theme = null;
     const jsonFilePath = token.filePath;
@@ -27,38 +19,18 @@ StyleDictionary.registerTransform({
       theme = 'paper';
     }
 
-    // console.log(token);
-    // console.log(theme);
-    // console.log(tokenPath);
-
     if (theme) {
       tokenPath.splice(1, 0, theme);
     }
-
-    console.log(tokenPath.join('-'));
-
-    // $vf-color-light-link-default
 
     return tokenPath.join('-');
   },
 });
 
-// REGISTER THE CUSTOM TRANSFORM GROUPS
-
-// if you want to see what a pre-defined group contains, uncomment the next line:
-// console.log(StyleDictionary.transformGroup['group_name']);
-
-// StyleDictionary.registerTransformGroup({
-//   name: 'custom/scss',
-//   transforms: ['attribute/cti', 'name/cti/kebab', 'time/seconds', 'content/icon', 'size/rem', 'color/css', 'name/squiggle'],
-// });
-
 // List of scss transforms acquired from:
 // https://github.com/amzn/style-dictionary/blob/main/lib/common/transformGroups.js
 StyleDictionary.registerTransformGroup({
-  name: 'custom/scss',
-  // this is to show one possibility for adding a few transforms to a pre-defined group
-  // (however, we suggest to use the previous approach, which is more explicit and clear)
+  name: 'vanilla/scss',
   transforms: [
     'attribute/cti',
     'name/kebab',
@@ -74,24 +46,19 @@ StyleDictionary.registerTransformGroup({
     'typography/css/shorthand',
     'transition/css/shorthand',
     'shadow/css/shorthand',
-    'name/squiggle',
+    'name/theme',
   ],
 });
 
-// APPLY THE CONFIGURATION
-// IMPORTANT: the registration of custom transforms
-// needs to be done _before_ applying the configuration
-// const styleDictionary = new StyleDictionary(__dirname + '/sd.config.json');
-
-const modes = ['light', 'dark', 'paper'];
-
 const styleDictionary = new StyleDictionary();
+
+console.log(`🏠 Building base variables...`);
 
 const baseSd = await styleDictionary.extend({
   source: [`tokens/color/base.json`],
   platforms: {
     scss: {
-      transformGroup: 'custom/scss',
+      transformGroup: 'vanilla/scss',
       prefix: 'vf',
       buildPath: 'build/scss/',
       files: [
@@ -106,20 +73,21 @@ const baseSd = await styleDictionary.extend({
 
 await baseSd.buildAllPlatforms();
 
-console.log(`☀️ Building light mode...`);
+console.log(`🌞 Building light theme variables...`);
 
 const lightSd = await styleDictionary.extend({
   include: [`tokens/color/base.json`],
   source: [`tokens/color/light/*.json`],
   platforms: {
     scss: {
-      transformGroup: 'custom/scss',
+      transformGroup: 'vanilla/scss',
       prefix: 'vf',
       buildPath: 'build/scss/',
       files: [
         {
           destination: 'tokens.light.scss',
           format: 'scss/variables',
+          filter: (token) => token.path[1] === 'light',
         },
       ],
     },
@@ -128,18 +96,21 @@ const lightSd = await styleDictionary.extend({
 
 await lightSd.buildAllPlatforms();
 
+console.log(`🌚 Building dark theme variables...`);
+
 const darkSd = await styleDictionary.extend({
   include: [`tokens/color/base.json`],
   source: [`tokens/color/dark/*.json`],
   platforms: {
     scss: {
-      transformGroup: 'custom/scss',
+      transformGroup: 'vanilla/scss',
       prefix: 'vf',
       buildPath: 'build/scss/',
       files: [
         {
           destination: 'tokens.dark.scss',
           format: 'scss/variables',
+          filter: (token) => token.path[1] === 'dark',
         },
       ],
     },
@@ -148,5 +119,5 @@ const darkSd = await styleDictionary.extend({
 
 await darkSd.buildAllPlatforms();
 
-console.log('\n==============================================');
-console.log('\nBuild completed!');
+console.log('==============================================');
+console.log('Style Dictionary build completed!');
