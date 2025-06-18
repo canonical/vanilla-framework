424ada9afca50ca9d807b0e1d7d83064ea739041
Julie Muzina <julie.muzina@canonical.com>
2025-06-18 15:56:30 -0400

    start charm migration - it packs!

---
+++ app.py
@@ -0,0 +1,3 @@
+# This is used by Rockcraft, which expects `app.py` in the project root.
+# https://documentation.ubuntu.com/rockcraft/en/stable/reference/extensions/flask-framework/#project-requirements
+from webapp.app import app
