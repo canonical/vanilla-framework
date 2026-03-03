99e81f6ebc759cfbff4d7772d43035cf1f916679
Peter French <peterfrench94@gmail.com>
2026-03-03 08:31:36 +0100

    chore: bump parker.js stylesheet threshold to 532480

--- tests/parker.js
+++ tests/parker.js
@@ -12,7 +12,7 @@ function generateMetrics(file, metricsArray) {
     {
       name: 'Stylesheet size',
       benchmark: 150000,
-      threshold: 522850,
+      threshold: 532480,
       result: results['total-stylesheet-size'],
     },
     {
