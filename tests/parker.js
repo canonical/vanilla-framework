548260a4a2fb11e62c54d2fd83cace9c90ae5ae6
Shahir Kazi <shahirmuktasid22@gmail.com>
2026-04-07 17:01:53 +0300

    fix: max stylesheet size

--- tests/parker.js
+++ tests/parker.js
@@ -12,7 +12,7 @@ function generateMetrics(file, metricsArray) {
     {
       name: 'Stylesheet size',
       benchmark: 150000,
-      threshold: 550300,
+      threshold: 553301,
       result: results['total-stylesheet-size'],
     },
     {
